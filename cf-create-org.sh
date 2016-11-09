a9bf69de7ea8afbc8377eee4a52117794c69c8f5
Aidan Feldman <aidan.feldman@gsa.gov>
2016-11-09 11:28:24 -0500

    validate arguments for org creation

--- cf-create-org.sh
+++ cf-create-org.sh
@@ -15,8 +15,32 @@ MANAGER=$5
 
 MEMORY="${6:-4G}"
 
+if ! [[ $AGENCY_NAME =~ ^[a-zA-Z0-9]+$ ]]; then
+  echo "AGENCY_NAME must contain only letters and numbers."
+  exit 1
+fi
+
+if ! [[ $BIZ_ID =~ ^[a-zA-Z0-9_-]+$ ]]; then
+  echo "BIZ_ID must contain only letters, numbers, underscores, and hyphens."
+  exit 1
+fi
+
+if ! [[ $SYSTEM_NAME =~ ^[a-zA-Z0-9-]+$ ]]; then
+  echo "SYSTEM_NAME must contain only letters, numbers, and hyphens."
+  exit 1
+fi
+
+if ! [[ $NOTE =~ ^[a-zA-Z0-9_-]+$ ]]; then
+  echo "NOTE must contain only letters, numbers, underscores, and hyphens."
+  exit 1
+fi
+
 QUOTA_NAME="${AGENCY_NAME}_${BIZ_ID}_${NOTE}"
+# uppercase
+QUOTA_NAME=$(echo $QUOTA_NAME | awk '{print toupper($0)}')
 ORG_NAME="${AGENCY_NAME}-${SYSTEM_NAME}"
+# lowercase
+ORG_NAME=$(echo $ORG_NAME | awk '{print tolower($0)}')
 NUMBER_OF_ROUTES=10
 NUMBER_OF_SERVICES=10
 

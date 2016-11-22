2e02982db8d4173d9f65915f17313d3908891a61
Aidan Feldman <aidan.feldman@gsa.gov>
2016-11-22 15:45:03 -0500

    refactor Ruby scripts
    * Various formatting cleanup, including fixing indentation to use spaces rather than tabs, which is a convention for Ruby.
    * Share the CF CLI logic between the scripts.

--- cf-orgsinfo.rb
+++ cf-orgsinfo.rb
@@ -1,20 +1,15 @@
 #!/usr/bin/env ruby
 # 2016 Dan Shick for 18F/GSA
-# 
+#
 # cf-orgsinfo -- outputs a CSV of Cloud Foundry organizations, GUIDs,
 # associated services and all org managers.
 # You must be authenticated to a CF API for this to work, as it uses
 # "cf curl".
 
-require 'rubygems'
-require 'json'
 require 'csv'
 require 'pp'
+require_relative 'helpers'
 
-def cfmunge(url)
-  raw = %x(/usr/local/bin/cf curl "#{url}")
-  return cooked = JSON.parse(raw)["resources"]
-end
 
 stamp = Time.now.to_i
 
@@ -22,22 +17,22 @@ CSV.open("cf-orgs-services-managers-#{stamp}.csv", "wb") do |csv|
   csv << ["Name","Org ID","Managers"]
 end
 
-#cfmunge('/v2/organizations?q=name:18f-acq').each do |r|
+#Helpers.cfmunge('/v2/organizations?q=name:18f-acq').each do |r|
 # for testing, filter out just one org
 
 # loop 1: get all orgs
-cfmunge('/v2/organizations?q=name:18f-acq').each do |r|
+Helpers.cfmunge('/v2/organizations?q=name:18f-acq').each do |r|
   name = r["entity"]["name"]
   org = r["metadata"]["guid"]
   boundservicesall = Array.new
   plans = Array.new
 
 # loop 2: get all apps for the org
-  cfmunge("/v2/apps?q=organization_guid:#{org}").each do |app|
+  Helpers.cfmunge("/v2/apps?q=organization_guid:#{org}").each do |app|
 
 # loop 3: for each app, get its service bindings and use them to create a list of bound services
 # not user-provided
-    cfmunge("/v2/service_bindings?q=app_guid:#{app["metadata"]["guid"]}").each do |bind|
+    Helpers.cfmunge("/v2/service_bindings?q=app_guid:#{app["metadata"]["guid"]}").each do |bind|
       boundservicesall.push(bind["entity"]["service_instance_guid"]) unless bind["entity"]["service_instance_url"].match('user_provided_service_instances')
 
     end
@@ -47,11 +42,11 @@ cfmunge('/v2/organizations?q=name:18f-acq').each do |r|
 
     boundservices = boundservicesall.uniq
     boundservices.each do |serv|
-      plans.push(cfmunge("/v2/service_plans?q=service_instance_guid:#{serv}").first["entity"]["name"])
+      plans.push(Helpers.cfmunge("/v2/service_plans?q=service_instance_guid:#{serv}").first["entity"]["name"])
    end
   end
   managers = Array.new
-  cfmunge("/v2/organizations/#{org}/managers").each do |m|
+  Helpers.cfmunge("/v2/organizations/#{org}/managers").each do |m|
     managers.push(m["entity"]["username"])
   end
   CSV.open("cf-orgs-services-managers-#{stamp}.csv", "ab") do |csv|
--- cf-space-serviceplans.rb
+++ cf-space-serviceplans.rb
@@ -1,90 +1,71 @@
 #!/usr/bin/env ruby
 # 2016 Dan Shick for 18F/GSA
 #
-# cf-orgsinfo -- outputs a CSV of Cloud Foundry organizations, GUIDs,
-# associated services and all org managers.
 # You must be authenticated to a CF API for this to work, as it uses
 # "cf curl".
 
-require 'rubygems'
-require 'json'
-require 'csv'
 require 'pp'
+require_relative 'helpers'
 
-def cfmunge(url)
-  raw = %x(/usr/local/bin/cf curl "#{url}")
-  return cooked = JSON.parse(raw)["resources"]
-end
 
 def get_all_services
+  services = Hash.new
+  Helpers.cfmunge('/v2/services?results-per-page=100').each do |service|
+    services[service['metadata']['guid']] = service['entity']['description']
+  end
 
-	services = Hash.new
-	cfmunge('/v2/services?results-per-page=100').each do |service|
-		services[service['metadata']['guid']] = service['entity']['description']
-	end
-
-	services
+  services
 end
 
 def get_used_service_plans(services)
+  service_plans = Hash.new
 
-	service_plans = Hash.new
+  Helpers.cfmunge('/v2/service_plans?results-per-page=100').each do |service_plan|
+    service_plans[service_plan['metadata']['guid']] = {
+      'service_guid' => service_plan['entity']['service_guid'],
+      'expanded_name' => services[service_plan['entity']['service_guid']] + ' : ' + service_plan['entity']['name'],
+      'service_instance_count' => 0
+    }
+  end
 
-	cfmunge('/v2/service_plans?results-per-page=100').each do |service_plan|
-		service_plans[service_plan['metadata']['guid']] = {
-			'service_guid' => service_plan['entity']['service_guid'],
-			'expanded_name' => services[service_plan['entity']['service_guid']] + ' : ' + service_plan['entity']['name'],
-			'service_instance_count' => 0
-		}
-	end
+  page = 1
+  while page < 3
+    Helpers.cfmunge("/v2/service_instances?page=#{page}&results-per-page=100").each do |service_instance|
+      service_plans[service_instance['entity']['service_plan_guid']]['service_instance_count'] += 1
+    end
+    page += 1
+  end
 
-	page = 1
-	while page < 3
-		cfmunge("/v2/service_instances?page=#{page}&results-per-page=100").each do |service_instance|
-			service_plans[service_instance['entity']['service_plan_guid']]['service_instance_count'] += 1
-		end
-		page += 1
-	end
-
-	# reduce the service plans hash to only those plans that have active instances, and sort them
-	# in descenting order
-	service_plans.delete_if {| k, v | v['service_instance_count'] == 0 }
-	service_plans = service_plans.sort_by { |k, v| v['service_instance_count'] }.reverse
+  # reduce the service plans hash to only those plans that have active instances, and sort them
+  # in descenting order
+  service_plans.delete_if {| k, v | v['service_instance_count'] == 0 }
+  service_plans = service_plans.sort_by { |k, v| v['service_instance_count'] }.reverse
 
 end
 
 def get_service(service_guid)
-
-  raw = %x(/usr/local/bin/cf curl "/v2/services/#{service_guid}")
-  return cooked = JSON.parse(raw)
-
+  Helpers.cfmunge("/v2/services/#{service_guid}")
 end
 
 
 def get_service_plan(service_plan_guid)
-
-  raw = %x(/usr/local/bin/cf curl "/v2/service_plans/#{service_plan_guid}")
-  return cooked = JSON.parse(raw)
-
+  Helpers.cfmunge("/v2/service_plans/#{service_plan_guid}")
 end
 
 def get_org_managers(org_guid)
+  managers = []
 
-  managers = Array.new
-
-  cfmunge("/v2/organizations/#{org_guid}/managers").each do |m|
+  Helpers.cfmunge("/v2/organizations/#{org_guid}/managers").each do |m|
     managers.push(m["entity"]["username"])
   end
 
   managers.join(",")
-
 end
 
 def get_space_managers(space_guid)
+  developers = []
 
-  developers = Array.new
-
-  cfmunge("/v2/spaces/#{space_guid}/developers").each do |m|
+  Helpers.cfmunge("/v2/spaces/#{space_guid}/developers").each do |m|
     developers.push(m["entity"]["username"])
   end
 
@@ -93,46 +74,43 @@ def get_space_managers(space_guid)
 end
 
 def get_service_plan_headers(service_plans)
+  headers = []
+  service_plans.each do |k, v|
+    headers.push(v['expanded_name'])
+  end
 
-	headers = Array.new
-	service_plans.each do |k, v|
-		headers.push(v['expanded_name'])
-	end
-
-	headers
+  headers
 end
 
 def get_space_service_instances(space_guid, service_plans)
+  service_instances = Hash.new
 
-	service_instances = Hash.new
+  Helpers.cfmunge("/v2/spaces/#{space_guid}/service_instances").each do |service_instance|
+    if service_instances.key?(service_instance['entity']['service_plan_guid'])
+      service_instances[service_instance['entity']['service_plan_guid']] += 1
+    else
+      service_instances[service_instance['entity']['service_plan_guid']] = 0
+    end
+  end
 
-	cfmunge("/v2/spaces/#{space_guid}/service_instances").each do |service_instance|
-		if service_instances.key?(service_instance['entity']['service_plan_guid'])
-			service_instances[service_instance['entity']['service_plan_guid']] += 1
-		else
-			service_instances[service_instance['entity']['service_plan_guid']] = 0
-		end
-	end
+  instances = []
+  service_plans.each do |k, v|
+    if service_instances.key?(k)
+      instances.push("#{service_instances[k]}")
+    else
+      instances.push("0")
+    end
+  end
 
-	instances = Array.new
-	service_plans.each do |k, v|
-		if service_instances.key?(k)
-			instances.push("#{service_instances[k]}")
-		else
-			instances.push("0")
-		end
-	end
-	instances
-
+  instances
 end
 
 def display_space_services(space_guid)
-	cfmunge("/v2/spaces/#{space_guid}/service_instances").each do |service_instance|
-		service_plan = get_service_plan(service_instance['entity']['service_plan_guid'])
-		service = get_service(service_plan['entity']['service_guid'])
-		puts "#{service['entity']['description']} #{service_plan['entity']['name']} : #{service_plan['entity']['description']}"
-	end
+  Helpers.cfmunge("/v2/spaces/#{space_guid}/service_instances").each do |service_instance|
+    service_plan = get_service_plan(service_instance['entity']['service_plan_guid'])
+    service = get_service(service_plan['entity']['service_guid'])
+    puts "#{service['entity']['description']} #{service_plan['entity']['name']} : #{service_plan['entity']['description']}"
+  end
 end
 
 display_space_services('bd4bbca2-f3f5-4ba6-808d-6e42469201b0')
-

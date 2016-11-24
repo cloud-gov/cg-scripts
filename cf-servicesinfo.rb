82aa53f24e1d50af72a47d097b0f17b3d057cbda
Aidan Feldman <aidan.feldman@gsa.gov>
2016-11-23 18:34:02 -0600

    move pagination logic to its own function

--- cf-servicesinfo.rb
+++ cf-servicesinfo.rb
@@ -14,25 +14,18 @@ def exempt_space(space_name)
 end
 
 
-def get_non_sandbox_spaces()
+def get_non_sandbox_spaces
   all_spaces = {}
 
-  Helpers.cfmunge('/v2/organizations?results-per-page=100').each do |org|
+  Helpers.cf_api_paginated('/v2/organizations').each do |org|
     next if org['entity']['name'].include? "sandbox"
 
-    page = 1
-    more_spaces = true
-    while more_spaces
-      spaces = Helpers.cfmunge("/v2/organizations/#{org['metadata']['guid']}/spaces?page=#{page}&results-per-page=100")
-      if spaces.length > 0
-        spaces.each do |space|
-          next if exempt_space(space['entity']['name'])
-          all_spaces[space['metadata']['guid']] = space['entity']['name']
-        end
-      else
-        more_spaces = false
-      end
-      page += 1
+    org_guid = org['metadata']['guid']
+    Helpers.cf_api_paginated("/v2/organizations/#{org_guid}/spaces").each do |space|
+      space_name = space['entity']['name']
+      next if exempt_space(space_name)
+      space_guid = space['metadata']['guid']
+      all_spaces[space_guid] = space_name
     end
   end
 
@@ -44,7 +37,7 @@ end
 
 def get_all_services
   services = {}
-  Helpers.cfmunge('/v2/services?results-per-page=100').each do |service|
+  Helpers.cf_api_paginated('/v2/services').each do |service|
     services[service['metadata']['guid']] = service['entity']['description']
   end
 
@@ -54,7 +47,7 @@ end
 def get_used_service_plans(services, spaces)
   service_plans = {}
 
-  Helpers.cfmunge('/v2/service_plans?results-per-page=100').each do |service_plan|
+  Helpers.cf_api_paginated('/v2/service_plans').each do |service_plan|
     service_plans[service_plan['metadata']['guid']] = {
       'service_guid' => service_plan['entity']['service_guid'],
       'expanded_name' => services[service_plan['entity']['service_guid']] + ' : ' + service_plan['entity']['name'],
@@ -62,14 +55,10 @@ def get_used_service_plans(services, spaces)
     }
   end
 
-  page = 1
-  while page < 3
-    Helpers.cfmunge("/v2/service_instances?page=#{page}&results-per-page=100").each do |service_instance|
-      if spaces[service_instance['entity']['space_guid']]
-        service_plans[service_instance['entity']['service_plan_guid']]['service_instance_count'] += 1
-      end
+  Helpers.cf_api_paginated('/v2/service_instances').each do |service_instance|
+    if spaces[service_instance['entity']['space_guid']]
+      service_plans[service_instance['entity']['service_plan_guid']]['service_instance_count'] += 1
     end
-    page += 1
   end
 
   # reduce the service plans hash to only those plans that have active instances, and sort them
@@ -95,8 +84,9 @@ def get_space_managers(space_guid)
   managers = []
 
   Helpers.cfmunge("/v2/spaces/#{space_guid}/managers").each do |m|
-    next if !m["entity"]["username"]
-    managers.push(m["entity"]["username"])
+    username = m["entity"]["username"]
+    next if !username
+    managers.push(username)
   end
 
   managers.join(",")
@@ -107,16 +97,17 @@ def get_space_developers(space_guid)
   developers = []
 
   Helpers.cfmunge("/v2/spaces/#{space_guid}/developers").each do |m|
-    next if !m["entity"]["username"]
-    developers.push(m["entity"]["username"])
+    username = m["entity"]["username"]
+    next if !username
+    developers.push(username)
   end
 
   developers.join(",")
 end
 
 def get_service_plan_headers(service_plans)
-  service_plans.values.map do |plan|
-    plan['expanded_name']
+  service_plans.map do |k, v|
+    v['expanded_name']
   end
 end
 
@@ -147,21 +138,23 @@ stamp = Time.now.to_i
 CSV.open("cf-orgs-services-managers-#{stamp}.csv", "wb") do |csv|
   csv << ["Org","Org Managers","Space", "Space Developers", "Space Developers", "Space GUID"] + get_service_plan_headers(@service_plans)
 
-  Helpers.cfmunge('/v2/organizations?results-per-page=100&order-by=name').each do |org|
+  Helpers.cf_api_paginated('/v2/organizations?order-by=name').each do |org|
     next if org['entity']['name'].include? "sandbox"
 
     org_name = org['entity']['name']
-    org_managers = get_org_managers(org['metadata']['guid'])
+    org_guid = org['metadata']['guid']
+    org_managers = get_org_managers(org_guid)
 
-    Helpers.cfmunge("/v2/organizations/#{org['metadata']['guid']}/spaces").each do |space|
+    Helpers.cf_api_paginated("/v2/organizations/#{org_guid}/spaces").each do |space|
       next if exempt_space(space['entity']['name'])
 
       space_name = space['entity']['name']
-      space_managers = get_space_managers(space['metadata']['guid'])
-      space_developers = get_space_developers(space['metadata']['guid'])
+      space_guid = space['metadata']['guid']
+      space_managers = get_space_managers(space_guid)
+      space_developers = get_space_developers(space_guid)
 
-      row = ["#{org_name}","#{org_managers}","#{space_name}", "#{space_managers}", "#{space_developers}", "#{space['metadata']['guid']}"]
-      row += get_space_service_instances(space['metadata']['guid'], @service_plans)
+      row = [org_name, org_managers, space_name, space_managers, space_developers, space_guid]
+      row += get_space_service_instances(space_guid, @service_plans)
       csv << row
     end
   end

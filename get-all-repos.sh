f54c8afe2d7763677cbc7f839159653ef3f6f60c
Will Woodson <william.woodson@gsa.gov>
2017-05-04 13:47:52 -0500

    Update get-all-repos.sh to only print unique repositories. Add --help and --verbose flags.

--- get-all-repos.sh
+++ get-all-repos.sh
@@ -9,12 +9,27 @@ YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
 NC='\033[0m'
 
+if [[ -n $1 && $1 =~ (-h|--help)$ ]]
+then
+  echo -e "
+  ./get-all-repos [--help, -h] [--verbose, -v]
+
+  Get all unique GitHub respositories referenced by pipelines on a Concourse server.
+  
+  Requires an environment variable ${YELLOW}\$ci_env${NC} set matching ${YELLOW}\`fly targets\`${NC}
+  eg, ${PURPLE}ci_env=fr ./get-all-repos --verbose${NC}
+  "
+  exit
+fi
+
 if [[ -z $ci_env ]]
 then
-  echo -e "Please set a ${YELLOW}\$ci_env${NC} variable to continue from ${YELLOW}fly targets${NC}"
-  echo -e "eg, ${PURPLE}ci_env=fr ./generate-all-certificates.sh --grab-cert${NC}"
+  echo -e "Please set a ${YELLOW}\$ci_env${NC} variable to continue from ${YELLOW}\`fly targets\`${NC}"
+  echo -e "eg, ${PURPLE}ci_env=fr ./get-all-repos${NC}"
   exit 99
 fi
+
+repo_list=$( mktemp )
 
 pipelines=$(
 fly -t $ci_env pipelines | \
@@ -23,7 +38,7 @@ grep -Eo '^[a-z0-9\-]+'
 
 for pipeline in $pipelines
 do
-  repo_list=$(
+  pipeline_repo_list=$(
   fly -t $ci_env gp -p "$pipeline" | \
   grep -E 'uri.*github' | \
   grep -oE '\/[0-9a-zA-Z\-]+\/[A-Z0-9a-z\-]+(\.git)?' | \
@@ -32,6 +47,15 @@ do
   sed -e "s/\.git//" | \
   sed -e "s/\n//"
   )
-  echo "Found $( echo "${repo_list}" | wc -l ) repositories in ${pipeline}"
-  echo "${repo_list}"
+
+  if [[ -n $1 && $1 =~ (-v|--verbose)$ ]]
+  then
+    echo -e "${GREEN}Found${NC} $( echo "${pipeline_repo_list}" | wc -l ) repositories in ${YELLOW}${pipeline}${NC}"
+    echo -e "${pipeline_repo_list}\n-----"
+  fi
+  
+  echo "${pipeline_repo_list}" >> $repo_list
 done
+
+cat $repo_list | sort -u
+rm $repo_list

2d29fb73b05283ea1a385ea3d9fb87cec4f20ffd
DKP <david.kane-parry@gsa.gov>
2017-05-04 16:14:37 -0700

    Delineate Repository Ownership
    To manage security, we need to understand where our internal processes and external dependencies are.  This script extracts all of the github URIs from ConcourseCI pipelines, sorts and deduplicates them, and then buckets them into repos that are under the 18F organization and repos that are not.

--- get-all-repos.sh
+++ get-all-repos.sh
@@ -2,60 +2,58 @@
 
 set -e
 
-RED='\033[0;31m'
-CYAN='\033[0;36m'
-GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
 NC='\033[0m'
 
-if [[ -n $1 && $1 =~ (-h|--help)$ ]]
-then
-  echo -e "
-  ./get-all-repos [--help, -h] [--verbose, -v]
-
-  Get all unique GitHub respositories referenced by pipelines on a Concourse server.
-  
-  Requires an environment variable ${YELLOW}\$ci_env${NC} set matching ${YELLOW}\`fly targets\`${NC}
-  eg, ${PURPLE}ci_env=fr ./get-all-repos --verbose${NC}
-  "
-  exit
-fi
-
 if [[ -z $ci_env ]]
 then
-  echo -e "Please set a ${YELLOW}\$ci_env${NC} variable to continue from ${YELLOW}\`fly targets\`${NC}"
-  echo -e "eg, ${PURPLE}ci_env=fr ./get-all-repos${NC}"
+  echo -e "Please set a ${YELLOW}\$ci_env${NC} variable to continue from \
+${YELLOW}fly targets${NC}"
+  echo -e "eg, ${PURPLE}ci_env=fr ./generate-all-certificates.sh \
+--grab-cert${NC}"
   exit 99
 fi
 
-repo_list=$( mktemp )
-
 pipelines=$(
-fly -t $ci_env pipelines | \
+fly -t "$ci_env" pipelines | \
 grep -Eo '^[a-z0-9\-]+'
 )
 
+repositories=$(
 for pipeline in $pipelines
 do
-  pipeline_repo_list=$(
-  fly -t $ci_env gp -p "$pipeline" | \
+  fly -t "$ci_env" gp -p "$pipeline" | \
   grep -E 'uri.*github' | \
   grep -oE '\/[0-9a-zA-Z\-]+\/[A-Z0-9a-z\-]+(\.git)?' | \
   uniq | \
   awk '{ print tolower($0) }' | \
   sed -e "s/\.git//" | \
   sed -e "s/\n//"
-  )
+done
+)
 
-  if [[ -n $1 && $1 =~ (-v|--verbose)$ ]]
-  then
-    echo -e "${GREEN}Found${NC} $( echo "${pipeline_repo_list}" | wc -l ) repositories in ${YELLOW}${pipeline}${NC}"
-    echo -e "${pipeline_repo_list}\n-----"
+repositories=$(echo "${repositories}" | tr " " "\n" | sort | uniq)
+our_repo=''
+not_repo=''
+
+for repo in $repositories
+do
+  if [[ $repo =~ ^\/18f\/ ]] ; then
+    our_repo="${our_repo}${repo} "
+  else
+    not_repo="${not_repo}${repo} "
   fi
-  
-  echo "${pipeline_repo_list}" >> $repo_list
 done
 
-cat $repo_list | sort -u
-rm $repo_list
+our_repo="${our_repo%"${our_repo##*[![:space:]]}"}"
+not_repo="${not_repo%"${not_repo##*[![:space:]]}"}"
+
+echo "-----------------------------"
+echo "GitHub Repositories Under 18F"
+echo "-----------------------------"
+echo "${our_repo}" | tr " " "\n"
+echo "---------------------------------"
+echo "GitHub Repositories NOT Under 18F"
+echo "---------------------------------"
+echo "${not_repo}" | tr " " "\n"

cb002c1db720b886952fe1a22f9e5246a52e0448
timothy-spencer <timothy.spencer@gsa.gov>
2018-07-20 16:01:40 -0700

    added audit mode and made it so that the return value might be useful

--- cloudtrail-check.sh
+++ cloudtrail-check.sh
@@ -12,21 +12,35 @@
 #     pulled dynamically from AWS and also if we handled cidr notation
 #     properly.  But these two ranges seem to work for govcloud right now.
 
+if [ "$1" = "-a" ] ; then
+	AUDITMODE=true
+fi
+RETURN=0
+
 START=$(date -v -15d +"%Y-%m-%dT%H:%M:%SZ")
-echo "pulling cloudtrail from a bit over 2 weeks ago ($START)"
+echo "pulling cloudtrail from a 15 days ago so that 2 week audits will always overlap a bit ($START)"
 
 aws cloudtrail lookup-events --no-paginate --start-time "$START" --lookup-attributes AttributeKey=EventName,AttributeValue=ConsoleLogin > /tmp/consolefoo.$$
 
-echo "============ Console users (run 'aws cloudtrail lookup-events --no-paginate --lookup-attributes AttributeKey=EventName,AttributeValue=ConsoleLogin' for details)"
+echo "============ Console logins per user"
+echo "============ Run 'aws cloudtrail lookup-events --no-paginate --lookup-attributes AttributeKey=EventName,AttributeValue=ConsoleLogin'"
+echo "============ for details on the logins if something seems unusual about the users or the number of times they logged in."
 cat /tmp/consolefoo.$$ | jq -r .Events[].Username | sort | uniq -c
 echo
 
-echo "============ Console events for Administrator (should be none)"
 cat /tmp/consolefoo.$$ | jq -r '.Events[] | select(.Username == "Administrator")' > /tmp/admin.$$
 cat /tmp/consolefoo.$$ | jq -r '.Events[] | select(.Username != "..*") | .CloudTrailEvent' | \
 	jq -r 'select(.userIdentity.userName == "Administrator")' >> /tmp/admin.$$
-cat /tmp/admin.$$
-echo
+if [ -s /tmp/admin.$$ ] ; then
+	echo "============ Found console events for Administrator: investigate"
+	cat /tmp/admin.$$
+	RETURN=1
+	echo
+elif [ "$AUDITMODE" = "true" ] ; then
+	echo "============ Checking console events for Administrator"
+	echo "none found:  no action required"
+	echo
+fi
 
 rm -f /tmp/admin.$$
 rm -f /tmp/consolefoo.$$
@@ -46,10 +60,22 @@ EVENTNAMES="
 "
 
 for event_name in $EVENTNAMES ; do
-	echo "======================= non-terraform events for $event_name (should be none)"
+	rm -rf /tmp/eventinfo.$$
 	aws cloudtrail lookup-events --no-paginate --start-time "$START" --lookup-attributes AttributeKey=EventName,AttributeValue="$event_name" | \
 		jq -r '.Events[] | .CloudTrailEvent' | \
-		jq -r 'select(if ((.userIdentity.accessKeyId | startswith("ASIA")) and (.sourceIPAddress | startswith("96.127") or startswith("52.222")) and (.userIdentity.principalId | test("[A-Z0-9]{21}:i-[0-9a-z]{17}"))) then false else true end)'
-	echo
+		jq -r 'select(if ((.userIdentity.accessKeyId | startswith("ASIA")) and (.sourceIPAddress | startswith("96.127") or startswith("52.222")) and (.userIdentity.principalId | test("[A-Z0-9]{21}:i-[0-9a-z]{17}"))) then false else true end)' > /tmp/eventinfo.$$
+	if [ -s /tmp/eventinfo.$$ ] ; then
+		echo "======================= found non-terraform events for $event_name: investigate"
+		cat /tmp/eventinfo.$$
+		RETURN=1
+		echo
+	elif [ "$AUDITMODE" = "true" ] ; then
+		echo "============ checking non-terraform events for $event_name"
+		echo "none found:  no action required"
+		echo
+	fi
+	rm -rf /tmp/eventinfo.$$
 done
+
+exit $RETURN
 

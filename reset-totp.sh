f08f64467da169c1ca7dbe1a94cb01289ba01ec9
Joshua Carp <jm.carp@gmail.com>
2018-08-06 10:59:45 -0400

    Add reset totp script.

---
+++ reset-totp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+if [ "$#" -ne 2 ]; then
+  script=$(basename "$0")
+  echo "Usage: ${script} <deployment> <username>"
+  exit 1
+fi
+
+deployment=$1
+totp_username=$2
+
+manifest=$(mktemp)
+bosh -d ${deployment} manifest > "${manifest}"
+address=$(bosh interpolate "${manifest}" --path /instance_groups/name=uaa/jobs/name=uaa/properties/uaadb/address)
+password=$(bosh interpolate "${manifest}" --path /instance_groups/name=uaa/jobs/name=uaa/properties/uaadb/roles/name=cfdb/password)
+rm "${manifest}"
+
+psql "postgres://cfdb:${password}@${address}:5432/uaadb" -c "delete from totp_seed where username = '${totp_username}'"

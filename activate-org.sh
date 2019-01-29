55d74e7208d4c8e5ca8c4bf85ddb8c08c19eee97
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2019-01-29 15:07:59 -0500

    Add new scripts to suspend and activate better

--- activate-org.sh
+++ activate-org.sh
@@ -5,7 +5,7 @@ set -e -x
 if [ "$#" -ne 1 ]; then
   echo
   echo "Usage:"
-  echo "   ./suspend-org.sh <org name>"
+  echo "   ./activate-org.sh <org name>"
   echo
     exit 1
 fi
@@ -13,12 +13,12 @@ fi
 org_name=$1
 GUID=$(cf org "${org_name}" --guid)
 
-cf curl "/v2/organizations/${GUID}" -X PUT -d '{"status":"suspended"}'
+cf curl "/v2/organizations/${GUID}" -X PUT -d '{"status":"active"}'
 
 cf target -o "${org_name}"
 for space in $(cf spaces "${org_name}" | tail -n +4); do
   cf t -s "${space}" > /dev/null
   for application in $(cf apps | tail -n +5); do
-    cf stop "${application}"
+    cf start "${application}"
   done
 done

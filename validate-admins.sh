ac7e4e11ddd0bee93e6c46d79bcf49c33bb73e36
Mike Lloyd <mike.lloyd@gsa.gov>
2019-06-18 12:59:41 -0600

    added script to run for validating admins.
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

---
+++ validate-admins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+f [ "$#" -ne 2 ]; then
+  echo
+  echo "Usage:"
+  echo "   ./validate-admins.sh <uaa-target> <uaa-admin-client-secret>"
+  echo
+  echo "   EX:  ./validate-admins.sh login.fr.cloud.gov S3c4Et"
+  echo
+    exit 1
+fi
+
+uaac target $1
+uaac token client get admin -s $2
+
+echo -e "admins for admin ui\n"
+for uuid in $(uaac groups -a members "displayName eq 'admin_ui.admin'" | grep value | awk '{print $2}'); do 
+    uaac users -a username "id eq '${uuid}'" | grep username | awk '{print $2}'
+done
+
+echo -e "\n\nadmins for cloud_controller (cf)\n"
+for uuid in $(uaac groups -a members "displayName eq 'cloud_controller.admin'" | grep value | awk '{print $2}'); do 
+    uaac users -a username "id eq '${uuid}'" | grep username | awk '{print $2}'
+done
+
+echo -e "\n\nadmins for concourse\n"
+for uuid in $(uaac groups -a members "displayName eq 'concourse.admin'" | grep value | awk '{print $2}'); do 
+    uaac users -a username "id eq '${uuid}'" | grep username | awk '{print $2}'
+done

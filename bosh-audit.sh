ae096aef22b827604313cc49590889e6c932fd85
Ben Berry <ben.berry@gsa.gov>
2019-07-15 11:37:25 -0700

    don't duplicate calls output in json more quotes!

--- bosh-audit.sh
+++ bosh-audit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   echo
   echo "Usage:"
   echo "    $ ./bosh-audit.sh <after-date>"
@@ -8,13 +8,13 @@ if [ -z $1 ]; then
   exit 1;
 fi
 
-after=$1
-last_id=$(bosh events --after "${after}" --json | jq -r '.Tables[].Rows[-1].id' | cut -d\  -f 1)
-bosh events --after "${after}"
+after="$1"
+last_row=$(bosh events --after "${after}" --json | tee jq -r '.Tables[].Rows[-1].id' - )
+last_id=$(cut -d\  -f 1 < "${last_row}")
 last_last_id=0
 for i in {1..1000}; do 
     if [[ ${last_id} == ${last_last_id} ]]; then break; fi
     last_last_id="${last_id}"
-    last_id=$(bosh events --after "${after}" --before-id "${last_id}" --json | jq -r '.Tables[].Rows[-1].id' | cut -d\  -f 1)
-    bosh events --after "${after}" --before-id "${last_id}"
+    last_row=$(bosh events --after "${after}" --before-id "${last_id}" --json | tee jq -r '.Tables[].Rows[-1].id' - )
+    last_id=$(cut -d\  -f 1 < "${last_row}")
 done

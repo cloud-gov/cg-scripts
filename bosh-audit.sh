c4ff9025ac3b3e269d671745b5bd0a57ae0ec690
Ben Berry <ben.berry@gsa.gov>
2019-07-15 11:25:17 -0700

    quotes for spaces

--- bosh-audit.sh
+++ bosh-audit.sh
@@ -9,12 +9,12 @@ if [ -z $1 ]; then
 fi
 
 after=$1
-last_id=$(bosh events --after ${after} --json | jq -r '.Tables[].Rows[-1].id' | cut -d\  -f 1)
-bosh events --after ${after}
+last_id=$(bosh events --after "${after}" --json | jq -r '.Tables[].Rows[-1].id' | cut -d\  -f 1)
+bosh events --after "${after}"
 last_last_id=0
 for i in {1..1000}; do 
     if [[ ${last_id} == ${last_last_id} ]]; then break; fi
     last_last_id="${last_id}"
-    last_id=$(bosh events --after ${after} --before-id ${last_id} --json | jq -r '.Tables[].Rows[-1].id' | cut -d\  -f 1)
-    bosh events --after ${after} --before-id ${last_id}
+    last_id=$(bosh events --after "${after}" --before-id "${last_id}" --json | jq -r '.Tables[].Rows[-1].id' | cut -d\  -f 1)
+    bosh events --after "${after}" --before-id "${last_id}"
 done

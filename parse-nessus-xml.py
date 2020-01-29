3b57bb2f62e874515c670ccdcdffeaf15be301d2
Peter Burkholder <peter.burkholder@gsa.gov>
2020-01-29 12:51:19 -0500

    Parse nessus vulnerabilitis for sanity

---
+++ parse-nessus-xml.py
@@ -0,0 +1,40 @@
+import nessus_file_reader as nfr
+import os
+import glob
+import traceback
+import time
+import sys
+
+
+if len(sys.argv) == 1:
+    print('please provide a path to an XML ZAP report')
+    sys.exit(-1)
+    
+nessus_scan_file = sys.argv[1]
+
+root = nfr.file.nessus_scan_file_root_element(nessus_scan_file)
+file_name = nfr.file.nessus_scan_file_name_with_path(nessus_scan_file)
+file_size = nfr.file.nessus_scan_file_size_human(nessus_scan_file)
+print(f'File name: {file_name}')
+print(f'File size: {file_size}')
+print('')
+
+
+vulnids = {}
+for report_host in nfr.scan.report_hosts(root):
+    report_host_name = nfr.host.report_host_name(report_host)
+    for report_item in nfr.host.report_items(report_host):
+        plugin_id = int(nfr.plugin.report_item_value(report_item, 'pluginID'))
+        risk_factor = nfr.plugin.report_item_value(report_item, 'risk_factor')
+        plugin_name = nfr.plugin.report_item_value(report_item, 'pluginName')
+        id = '{}, Risk: {}, Plugin Name: {}, https://www.tenable.com/plugins/nessus/{}'.format(
+                                            plugin_id, risk_factor, plugin_name, plugin_id)
+        hosts = vulnids.get(id, [])
+        hosts.append(report_host_name)
+        vulnids[id] = hosts
+
+for key in vulnids:
+    if key.find("Risk: None") == -1 :
+        print(key)
+        for site in vulnids[key]:
+            print('\t{}'.format(site))

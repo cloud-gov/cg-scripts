a679787b145628acdacdbce32dd152a24f2da8e1
Peter Burkholder <peter.burkholder@gsa.gov>
2020-02-06 12:35:43 -0500

    Sort and add plugin ids

--- parse-owasp-zap-xml.py
+++ parse-owasp-zap-xml.py
@@ -13,15 +13,16 @@ for site in tree.findall('site'):
     sitename = site.attrib['name']
     if sitename.find('cloud.gov') != -1:
         for alert in site.findall('.//alertitem'):
-            id = '{}, CWE id {}, WASC id {}, Risk {}'.format(alert.find('name').text,
+            id = '{}, CWE id {}, WASC id {}, Risk {}, Plugin ID {}'.format(alert.find('name').text,
                                                     alert.find('cweid').text,
                                                     alert.find('wascid').text,
-                                                    alert.find('riskdesc').text)
+                                                    alert.find('riskdesc').text,
+                                                    alert.find('pluginid').text)
             sites = vulnids.get(id, [])
             sites.append(sitename)
             vulnids[id] = sites
 
-for key in vulnids:
+for key in sorted(vulnids):
     print(key)
     for site in vulnids[key]:
         print('\t{}'.format(site))

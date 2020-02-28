83896d70c2f4c4b64e198bf15f523798a3428332
Tammer Saleh <tammer@superorbit.al>
2020-02-28 22:04:56 +0000

    improve strip-user-org-and-space-roles.sh

--- strip-user-org-and-space-roles.sh
+++ strip-user-org-and-space-roles.sh
@@ -1,22 +1,42 @@
-#!/bin/bash -ex
+#!/usr/bin/env bash
 
-# User input
-# cmd.sh USER ORG SPACE
+set -euo pipefail
+shopt -s inherit_errexit
 
-USER="$1"
-ORG="$2"
-ORGROLE="OrgManager"
+main() {
+  [[ $# -eq 3 ]] || usage "Expected three arguments, got $#"
 
-cf org-users $ORG
-cf unset-org-role $USER $ORG $ORGROLE
+  local user="$1"
+  local org="$2"
+  local space="$3"
 
-SPACE="$3"
-SPACEROLE="SpaceManager"
+  cf unset-org-role "$user" "$org" OrgManager
 
-cf space-users $ORG $SPACE
+  cf space-users "$org" "$space"
 
-cf unset-space-role $USER $ORG $SPACE $SPACEROLE
-SPACEROLE="SpaceDeveloper"
-cf unset-space-role $USER $ORG $SPACE $SPACEROLE
-SPACEROLE="SpaceAuditor"
-cf space-users $ORG $SPACE
+  for space_role in SpaceManager SpaceDeveloper SpaceAuditor; do
+    cf unset-space-role "$user" "$org" "$space" "$space_role"
+  done
+
+  echo "Org users:"
+  cf org-users "$org"
+
+  echo "Space users:"
+  cf space-users "$org" "$space"
+}
+
+usage() {
+  [[ $# -gt 0 ]] && echo "ERROR: $*"
+  cat <<EOF
+  USAGE: $(basename "$0") USER ORG SPACE
+
+  Removes org and space roles for user.
+
+  Examples:
+
+    $(basename "$0") bob accounting dev
+EOF
+  exit 1
+}
+
+main "$@"

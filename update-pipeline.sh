7b9a7af356e31b9db9d08f5284052e8a934ee0e0
Tammer Saleh <tammer@superorbit.al>
2020-03-05 16:56:23 -0800

    Fix the update-pipeline.sh script

--- update-pipeline.sh
+++ update-pipeline.sh
@@ -5,71 +5,76 @@ shopt -s inherit_errexit
 
 main() {
   [[ "${BASH_VERSINFO:-0}" -ge 5 ]]     || usage "This script must be run with Bash 5.x"
-  [[ $# -ge 2 && $# -le 3 ]]            || usage "Expected two or three arguments, received $#"
+  [[ $# -ge 2 && $# -le 4 ]]            || usage "Expected between 2-4 arguments, received $#"
   [[ -v CONCOURSE_CREDENTIALS_BUCKET ]] || usage "Expected \$CONCOURSE_CREDENTIALS_BUCKET to be set"
 
   FLY_TARGET=$1
   PIPELINE_FILE=$2
-  if [[ -v $3 ]]; then
-    PIPELINE="$3"
-  else
-    PIPELINE=$(basename "$GIT_ROOT")
-  fi
-
   GIT_ROOT=$(git rev-parse --show-toplevel)
+  PIPELINE=${3:-$(basename "$GIT_ROOT")}
+
+  TS="$(date +"%Y%m%d%H%M%S%3N")"
   SECRETS_DIR="$GIT_ROOT/do-not-commit"
-  SECRETS_FILE="$SECRETS_DIR/$PIPELINE.yml"
+  SECRETS_FILE_NAME="${4:-$PIPELINE.yml}"
+  SECRETS_PATH="$SECRETS_DIR/$SECRETS_FILE_NAME"
+  SECRETS_BACKUP_PATH="${SECRETS_PATH}-$TS"
 
-  ci get-pipelines "$PIPELINE" || usage "Cannot determine pipeline name (guessed $PIPELINE)"
+  ci status > /dev/null || ci login
+  ci get-pipeline -p "$PIPELINE" > /dev/null 2>&1 || usage "Cannot find pipeline named $PIPELINE"
 
-  mkdir "$SECRETS_DIR"
-  aws s3 cp "$CONCOURSE_CREDENTIALS_BUCKET/$PIPELINE.yml" "$SECRETS_FILE"
+  mkdir -p "$SECRETS_DIR"
+  [[ -f $SECRETS_PATH ]] && cp "$SECRETS_PATH" "$SECRETS_BACKUP_PATH"
+  aws s3 cp "$CONCOURSE_CREDENTIALS_BUCKET/$SECRETS_FILE_NAME" "$SECRETS_PATH"
 
-  ci get-pipeline -p "$PIPELINE" > "$SECRETS_DIR/pipeline-backup-$(date +"%Y%m%d").yml"
+  ci get-pipeline -p "$PIPELINE" > "$SECRETS_DIR/pipeline-backup-$TS.yml"
 
-  if ci validate-pipeline -c "$PIPELINE_FILE" -l "$SECRETS_FILE" > /dev/null; then
-    ci set-pipeline -p "$PIPELINE" --config "$PIPELINE_FILE" -l "$SECRETS_FILE"
+  if ci validate-pipeline -c "$PIPELINE_FILE" -l "$SECRETS_PATH" > /dev/null; then
+    ci set-pipeline -p "$PIPELINE" --config "$PIPELINE_FILE" -l "$SECRETS_PATH"
   fi
-  echo "If you change the $SECRETS_FILE file, update the canonical creds:"
+  echo "If you change the $SECRETS_FILE_NAME file, update the canonical creds:"
   echo
-  echo "aws s3 cp --sse AES256 $SECRETS_FILE $CONCOURSE_CREDENTIALS_BUCKET/$PIPELINE.yml"
+  echo "aws s3 cp --sse AES256 $SECRETS_PATH $CONCOURSE_CREDENTIALS_BUCKET/$PIPELINE.yml"
 }
 
 ci() {
-  fly -t "$FLY_TARGET" "$@";
+  fly -t "$FLY_TARGET" "$@"
 }
 
 usage() {
-  [[ $# -gt 0 ]] && echo "  ERROR: $*"
+  [[ $# -gt 0 ]] && (echo "  ERROR: $*"; echo)
   cat <<EOF
-  USAGE: $(basename "$0") fly-target pipeline-file [pipeline-name]
+USAGE:
 
-  This script updates the given pipeline.  In particular, it:
+  $(basename "$0") fly-target pipeline-file [pipeline-name] [secret-file-name]
 
-  1. Downloads the secrets file from S3
-  2. Backs up the existing pipeline definition locally
-  3. Validates the new pipeline file
-  4. Uploads the pipeline to concourse
+This script updates the given pipeline.  In particular, it:
 
-  It requires that the name of the bucket holding the secret credentials is set
-  in the \$CONCOURSE_CREDENTIALS_BUCKET environment variable.
+1. Downloads the secrets file from S3
+2. Backs up the existing pipeline definition locally
+3. Validates the new pipeline file
+4. Uploads the pipeline to concourse
 
-  If you omit pipeline-name, then the name of the root git directory is used as
-  a best guess.
+It requires that the name of the bucket holding the secret credentials is set
+in the \$CONCOURSE_CREDENTIALS_BUCKET environment variable.
 
-  This script assumes that "do-not-commit" is configured in the project
-  .gitignore (or your global excludesfile).  See the documentation on
-  "core.excludesfile" for more info:
+If you omit pipeline-name, then the name of the root git directory is used as
+a best guess.
 
-    https://help.github.com/en/github/using-git/ignoring-files
+This script assumes that "do-not-commit" is configured in the project
+.gitignore (or your global excludesfile).  See the documentation on
+"core.excludesfile" for more info:
 
-  Examples:
+  https://help.github.com/en/github/using-git/ignoring-files
 
-    export CONCOURSE_CREDENTIALS_BUCKET=s3://mysekrits
+Examples:
 
-    $(basename "$0") concourse ./ci/pipeline.yml
+  export CONCOURSE_CREDENTIALS_BUCKET=s3://mysekrits
 
-    $(basename "$0") concourse pipeline.yml deploy-foo
+  $(basename "$0") concourse ./ci/pipeline.yml
+
+  $(basename "$0") concourse pipeline.yml deploy-foo
+
+  $(basename "$0") concourse pipeline.yml deploy-foo deploy-foo.yml
 EOF
   exit 1
 }

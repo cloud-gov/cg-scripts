dc91e91e10f00ae7728b527a0e9f484890977067
Mark Headd <mheadd@gmail.com>
2020-10-07 14:33:00 -0400

    Add global_auditor check to validate-admins.sh

--- validate-admins.sh
+++ validate-admins.sh
@@ -9,7 +9,7 @@ if [ "$#" -ne 2 ]; then
   echo 
   echo "   Obtain uaa-admin-client-secret by running:"
   echo 
-  echo "   credhub get -n \"/bosh/cf-{environment-name}/uaa_admin_client_secret\" | grep value | sed -r 's/value: //g'"
+  echo "   credhub get -n \"/bosh/cf-{environment-name}/uaa_admin_client_secret\" | grep value | sed -r 's/value: //g'\"
   echo
     exit 1
 fi
@@ -24,6 +24,11 @@ done
 
 echo -e "\n\nadmins for cloud_controller (cf)\n"
 for uuid in $(uaac groups -a members "displayName eq 'cloud_controller.admin'" | grep value | awk '{print $2}'); do 
+    uaac users -a username "id eq '${uuid}'" | grep username | awk '{print $2}'
+done
+
+echo -e "\n\nadmins for global_auditor (cf)\n"
+for uuid in $(uaac groups -a members "displayName eq 'cloud_controller.global_auditor'" | grep value | awk '{print $2}'); do 
     uaac users -a username "id eq '${uuid}'" | grep username | awk '{print $2}'
 done
 

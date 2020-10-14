eea2ff68c50e00c1af3b9b02ba64ebc0eb005272
Mark Headd <mheadd@gmail.com>
2020-10-14 10:28:46 -0400

    Add script to pull usernames/emails from sandbox accounts.

---
+++ audit/get-sandbox-user-emails.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get a list of all orgs names prepended with the word sandbox"
+orgs=($(cf orgs | grep '^sandbox'))
+
+echo "Username, Org GUID"
+
+# Loop through the array and look up quota details.
+for org in "${orgs[@]}"
+do
+    guid=$(cf org $org --guid)
+
+    emails=($(cf curl "/v2/organizations/$guid/users" | jq '.resources | .[].entity | .username'))
+    for email in "${emails[@]}"
+    do
+        echo "${email}, ${guid}"
+    done
+    sleep 1
+done

488c361b0697adf31cf06aaf41b5018b720abcbe
Peter Burkholder <peter.burkholder@gsa.gov>
2021-03-29 13:26:01 -0400

    Hide nessus output, note skipped sites

--- parse-owasp-zap-xml.py
+++ parse-owasp-zap-xml.py
@@ -17,12 +17,14 @@ for site in tree.findall('site'):
         for alert in site.findall('.//alertitem'):
             id = '{}, CWE id {}, WASC id {}, Risk {}, Plugin ID {}'.format(alert.find('name').text,
                                                     alert.find('cweid').text,
-                                                    alert.find('wascid').text,
+                                                    alert.findtext('wascid',"None"),
                                                     alert.find('riskdesc').text,
                                                     alert.find('pluginid').text)
             sites = vulnids.get(id, [])
             sites.append(sitename)
             vulnids[id] = sites
+    else:
+        print(f'Info - Skipping non-cloud.gov site: {sitename}', file=sys.stderr)
 
 for key in sorted(vulnids):
     print(key)

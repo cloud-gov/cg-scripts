f173fe4a190a52ef245a524c2ab93c98141163b1
Peter Burkholder <peter.burkholder@gsa.gov>
2021-11-02 10:44:11 -0400

    Add Rian, rm Kara and Becca

--- cloudybunch.py
+++ cloudybunch.py
@@ -3,22 +3,21 @@
 import random
 
 team = [
-    ":kara-reinsel:",
     ":amir:",
     ":david-corwin:",
     ":ben-berry:",
     ":peterb:",
     ":andrew-burnes:",
-    ":beccag:",
     ":van-nguyen:",
     ":chris-mcgowan:",
     ":kelley:",
     ":markheadd:",
     ":lindsay:",
     ":melanie:",
-    ":gerald:"
+    ":gerald:",
+    ":rian-bogle:"
 ]
 
-for n in range(1,20):
+for n in range(1,24):
     bunch = random.sample(team,8)
     print(f':musical_note: {bunch[0]} {bunch[1]} {bunch[2]}\\n :blank: {bunch[3]} :cloud-gov: {bunch[4]} \\n :blank: {bunch[5]} {bunch[6]} {bunch[7]} :musical_note:')

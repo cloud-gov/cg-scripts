57e2fb56c5b6e8db70639c19d9389ea8bce9d6d4
Carlo Costino <carlo.costino@gsa.gov>
2021-12-02 17:06:03 -0500

    Improve active sandbox and space count script
    This changeset adds a few improvements to the active sandbox and space count script by allowing an operator to specify start and end dates to filter by.  If a start date (and an end date) are given, the results are constrained to those dates in an inclusive fashion: starting at 00:00:00 on the start date and ending at 23:59:59 on the end date (if provided).
    
    Including the dates will also provide a new line of output, how many sandbox spaces were created during that time.

--- audit/get-active-sandbox-and-space-count.sh
+++ audit/get-active-sandbox-and-space-count.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Counts the number of current active sandboxes in the platform.
 
 # An active sandbox is defined as any user's sandbox space with 1 or more
@@ -8,7 +10,38 @@
 # We can determine this by using the cf CLI and CAPI to retrieve all sandbox
 # orgs, then filter a list of all apps with those corresponding org GUIDs.
 
+if [[ $1 == "--help" ]]; then
+  echo " "
+  echo "Counts the number of current active sandboxes in the platform."
+  echo " "
+	echo "Usage: get-active-sandbox-and-space-count [START_DATE] [END_DATE]"
+  echo " "
+  echo -e "  START_DATE: \t A date to limit finding active sandboxes at or after the provided date in YYYY-MM-DD format."
+  echo -e "  END_DATE: \t A date to limit finding active sandboxes at or before the provided date in YYYY-MM-DD format."
+  echo " "
+	exit 0
+fi
+
+start_date=$1
+end_date=$2
+extra_params=""
+
 echo "Getting total counts of active sandboxes and sandbox spaces..."
+
+# If a start date was given, filter for sandboxes created at the start date and
+# after, starting at the very beginning of the day.
+if [ $start_date ]; then
+    extra_params="${extra_params}&created_ats[gte]=${start_date}T00:00:00Z"
+    echo "    [Limiting search to sandboxes active starting on ${start_date} (00:00:00)]"
+fi
+
+# If an end date was given, filter for sandboxes created before the very end of
+# the day given and before.
+if [ $end_date ]; then
+    extra_params="${extra_params}&created_ats[lte]=${end_date}T23:59:59Z"
+    echo "    [Limiting search to sandboxes active no later than ${end_date} (23:59:59)]"
+fi
+
 echo
 
 # Get all of the sandbox orgs in a format to be used with a cf curl call.
@@ -24,12 +57,22 @@ sandbox_org_guids=$(cf curl "/v3/organizations?names=${sandbox_org_names}&per_pa
 echo "...Retrieving total number of spaces for all sandbox orgs..."
 total_sandbox_spaces=$(cf curl "/v3/spaces?organization_guids=${sandbox_org_guids}&per_page=5000" | jq -r '.pagination.total_results')
 
+# If we've set any dates, also check for spaces created during that time period.
+if [ $extra_params ]; then
+    echo "...Retrieving number of spaces created in all sandbox orgs during dates given..."
+    total_sandboxe_spaces_created=$(cf curl "/v3/spaces?organization_guids=${sandbox_org_guids}&per_page=5000${extra_params}" | jq -r '.pagination.total_results')
+fi
+
 # Retrieve the count of all running apps across all sandbox orgs, regardless of
 # the actual space they're running in.
 echo "...Retrieving total number of active sandboxes for all sandbox orgs..."
-total_active_sandboxes=$(cf curl "/v3/apps?organization_guids=${sandbox_org_guids}&per_page=5000" | jq -r '.pagination.total_results')
+total_active_sandboxes=$(cf curl "/v3/apps?organization_guids=${sandbox_org_guids}&per_page=5000${extra_params}" | jq -r '.pagination.total_results')
 
 # Print the final results.
 echo
-echo "Total number of sandbox spaces: $total_sandbox_spaces"
-echo "Total number of active sandboxes: $total_active_sandboxes"
+echo "Total number of sandbox spaces: ${total_sandbox_spaces}"
+echo "Total number of active sandboxes: ${total_active_sandboxes}"
+
+if [ $extra_params ]; then
+    echo "Total number of sandbox spaces created during given dates: ${total_sandboxe_spaces_created}"
+fi

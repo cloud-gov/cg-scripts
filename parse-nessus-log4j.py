1a69213461d366eb7850e8189e23131b07e3f3a6
Peter Burkholder <peter.burkholder@gsa.gov>
2022-02-01 14:56:32 -0500

    Add comments on how to use

--- parse-nessus-log4j.py
+++ parse-nessus-log4j.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# This emits the paths, and hosts, for log4j findings
+# For images with instance guids, like:
+#   ['cf-production-diego-cell-24-cf-production']
+#     /var/vcap/data/grootfs/store/unprivileged/images/395c7a88-7a1c-4001-55df-261d/diff/home/vcap/app/WEB-INF/lib/log4j-core-2.7.jar
+# then SSH to diego-cell/24, run 'cfdot cell-state (cell_id)' and 
+# use `jq` to find the process guid.  The first 36 chars are the app guid. 
+# 
+#    name, 467435c6c6612e0b91cc843ae331523636a9d7e4a4214213c8500abaa4d3024b
+#    in /var/vcap/data/grootfs/store/unprivileged/meta/dependencies.
+#    There should be a file named with the container id instance_guid,
+#    e.g. image:f34eb243-17e6-420d-73a7-2c39.json. Then you can use
+#    cfdot cell-state to get from instance_guid to the process_guid,
+#    and the first 36 chars of the process_guid is the app_id. from
+#    https://community.pivotal.io/s/article/How-to-find-which-Apps-are-Running-in-a-Diego-Cell?language=en_US
+#    with help from
+#    https://github.com/cloudfoundry/garden-runc-release/blob/develop/docs/understanding_grootfs_store_disk_usage.md
+
 import nessus_file_reader as nfr
 import os
 import glob

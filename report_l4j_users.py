b857e3cd92bd9aa37e85d0203db1822a85815995
Peter Burkholder <peter.burkholder@gsa.gov>
2022-02-02 12:11:38 -0500

    Proper CSV output

--- report_l4j_users.py
+++ report_l4j_users.py
@@ -3,6 +3,7 @@
 import json
 import os
 import subprocess
+import sys
 
 from cloudfoundry_client.client import CloudFoundryClient
 config_file = os.path.join(os.path.expanduser("~"), ".cf_client_python.json")
@@ -14,17 +15,30 @@ with open(config_file, "r") as f:
     client = CloudFoundryClient(configuration["target_endpoint"], verify=configuration["verify"])
     client.init_with_token(configuration["refresh_token"])
 
+import csv
+csvwriter = csv.writer(sys.stdout,quoting=csv.QUOTE_ALL)
+csvwriter.writerow([ "App Name", "Space Name", "Org Name", "Org Managers", "Space Devs" ])
+
 app_guid = "a4dafcbf-16a4-4ee4-a1a3-1fed3fb2402d"
 app = client.v3.apps.get(app_guid)
 space = app.space()
 org = space.organization()
-m= (f"\"{app['name']}\", \"{space['name']}\", \"{org['name']}\"")
 
-s = f"cf space-users {org['name']} {space['name']}"
-s = f"cf space-users gsa-forms-prototyping MiA" 
-p = ' | perl -ne \'if (/SPACE DEVELOPER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
-full_cmd = s + p
+sp_cf = f"cf space-users {org['name']} {space['name']}"
+sp_perl = ' | perl -ne \'if (/SPACE DEVELOPER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
+sp_cmd = sp_cf + sp_perl
+sp_out = subprocess.check_output(sp_cmd, shell=True, text=True)
 
-out = subprocess.check_output(full_cmd, shell=True, text=True)
+org_cf = f"cf org-users {org['name']}"
+org_perl = ' | perl -ne \'if (/ORG MANAGER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
+org_cmd = org_cf + org_perl
+org_out = subprocess.check_output(org_cmd, shell=True, text=True)
 
-print(m, ", \"", out, "\"" )
+csvwriter.writerow(
+    [ app['name'],
+      space['name'],
+      org['name'],
+      org_out,
+      sp_out
+    ])
+     

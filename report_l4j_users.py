5e0723aa2d4d859ae1ec4fde948c4bc952ddd227
Peter Burkholder <peter.burkholder@gsa.gov>
2022-02-05 15:05:08 -0500

    Clean up old code

--- report_l4j_users.py
+++ report_l4j_users.py
@@ -4,6 +4,7 @@ import json
 import os
 import subprocess
 import sys
+import re
 
 from cloudfoundry_client.client import CloudFoundryClient
 config_file = os.path.join(os.path.expanduser("~"), ".cf_client_python.json")
@@ -19,26 +20,40 @@ import csv
 csvwriter = csv.writer(sys.stdout,quoting=csv.QUOTE_ALL)
 csvwriter.writerow([ "App Name", "Space Name", "Org Name", "Org Managers", "Space Devs" ])
 
-app_guid = "a4dafcbf-16a4-4ee4-a1a3-1fed3fb2402d"
-app = client.v3.apps.get(app_guid)
-space = app.space()
-org = space.organization()
+app_guid_file = open("app_guids", "r")
+app_guid = app_guid_file.readline().strip()
+while app_guid:
+    if re.match(r'docker', app_guid ):
+        csvwriter.writerow(
+            [ "null",
+              "null",
+              "null",
+              "null",
+              "null"
+            ])
+    else:
+        app = client.v3.apps.get(app_guid)
+        space = app.space()
+        org = space.organization()
 
-sp_cf = f"cf space-users {org['name']} {space['name']}"
-sp_perl = ' | perl -ne \'if (/SPACE DEVELOPER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
-sp_cmd = sp_cf + sp_perl
-sp_out = subprocess.check_output(sp_cmd, shell=True, text=True)
+        sp_cf = f"cf space-users {org['name']} {space['name']}"
+        sp_perl = ' | perl -ne \'if (/SPACE DEVELOPER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
+        sp_cmd = sp_cf + sp_perl
+        sp_out = subprocess.check_output(sp_cmd, shell=True, text=True)
 
-org_cf = f"cf org-users {org['name']}"
-org_perl = ' | perl -ne \'if (/ORG MANAGER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
-org_cmd = org_cf + org_perl
-org_out = subprocess.check_output(org_cmd, shell=True, text=True)
+        org_cf = f"cf org-users {org['name']}"
+        org_perl = ' | perl -ne \'if (/ORG MANAGER/ .. /^$/) { next unless /@/; m/([a-z\.\]+@[a-z\.]+)/ and print "$1,"}\''
+        org_cmd = org_cf + org_perl
+        org_out = subprocess.check_output(org_cmd, shell=True, text=True)
 
-csvwriter.writerow(
-    [ app['name'],
-      space['name'],
-      org['name'],
-      org_out,
-      sp_out
-    ])
-     
+        csvwriter.writerow(
+            [ app['name'],
+            space['name'],
+            org['name'],
+            org_out,
+            sp_out
+            ])
+        
+    app_guid = app_guid_file.readline().strip()
+
+app_guid_file.close()   

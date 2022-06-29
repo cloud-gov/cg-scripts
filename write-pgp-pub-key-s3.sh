b9fc0829b371b3bda8356c48d2f1651f42725515
Mark Boyd <mark.boyd@gsa.gov>
2022-06-29 15:46:12 -0400

    remove timestamps from s3 files

--- write-pgp-pub-key-s3.sh
+++ write-pgp-pub-key-s3.sh
@@ -19,15 +19,10 @@ if [ $ec != 0 ]; then
     exit $ec
 fi
 
-timestamp=$(date +%s)
-
-# delete existing PGP keys for this user
-aws s3 rm s3://$PGP_KEYS_BUCKET --recursive --exclude "*" --include "$GIT_USER_NAME*.asc"
-
 PGP_TMP_FILE=$(mktemp)
 gpg --export --armor "$GIT_PGP_KEY_ID" > "$PGP_TMP_FILE"
 
 # upload PGP key to S3
-aws s3 cp --sse AES256 "$PGP_TMP_FILE" "s3://$PGP_KEYS_BUCKET/$GIT_USER_NAME-$timestamp.asc" || true
+aws s3 cp --sse AES256 "$PGP_TMP_FILE" "s3://$PGP_KEYS_BUCKET/$GIT_USER_NAME.asc" || true
 
 rm "$PGP_TMP_FILE"

bb6fd1bab116b598a6cc6ae764ec5e91b72cd3f4
Mark Boyd <mark.boyd@gsa.gov>
2022-06-29 14:56:50 -0400

    ensure script continues if s3 cp fails

--- write-pgp-pub-key-s3.sh
+++ write-pgp-pub-key-s3.sh
@@ -28,6 +28,6 @@ PGP_TMP_FILE=$(mktemp)
 gpg --export --armor "$GIT_PGP_KEY_ID" > "$PGP_TMP_FILE"
 
 # upload PGP key to S3
-aws s3 cp --sse AES256 "$PGP_TMP_FILE" "s3://$PGP_KEYS_BUCKET/$GIT_USER_NAME-$timestamp.asc"
+aws s3 cp --sse AES256 "$PGP_TMP_FILE" "s3://$PGP_KEYS_BUCKET/$GIT_USER_NAME-$timestamp.asc" || true
 
 rm "$PGP_TMP_FILE"

87341b1db4f15a144523f1ba391a1f03442241d6
Mark Boyd <mark.boyd@gsa.gov>
2022-06-29 16:46:49 -0400

    update script

--- write-pgp-pub-key-s3.sh
+++ write-pgp-pub-key-s3.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env bash
 
+function usage {
+    echo " "
+    echo "Copies PGP public key configured for git to S3"
+    echo " "
+    echo "Usage: $0 <aws-vault-profile>"
+    echo " "
+    echo -e "  aws-vault-profile:\tThe name of the aws-vault profile with admin privs in the account"
+    echo " "
+    echo "Options: "
+    echo " "
+    echo "    $0 -h                         Display this help message."
+    exit 0
+}
+
+while getopts ":h" opt; do
+  case ${opt} in
+    h )
+        usage
+        exit 0
+        ;;
+    * )
+        usage
+        exit 0
+        ;;
+  esac
+done
+
+AWS_VAULT_PROFILE=${1:-gov-prd-plat-admin}
+# if [ -z "$AWS_VAULT_PROFILE" ]; then
+#     # echo "Must specify an aws-vault profile as first argument to the script"
+#     usage
+#     exit 1
+# fi
+
+echo "Using aws-vault profile $AWS_VAULT_PROFILE"
+# aws-vault exec "$AWS_VAULT_PROFILE"
+
 PGP_KEYS_BUCKET="cg-pgp-keys"
 GIT_USER_NAME=$(git config user.name | tr ' ' '-')
 
@@ -23,6 +60,6 @@ PGP_TMP_FILE=$(mktemp)
 gpg --export --armor "$GIT_PGP_KEY_ID" > "$PGP_TMP_FILE"
 
 # upload PGP key to S3
-aws s3 cp --sse AES256 "$PGP_TMP_FILE" "s3://$PGP_KEYS_BUCKET/$GIT_USER_NAME.asc" || true
+aws-vault exec "$AWS_VAULT_PROFILE" -- aws s3 cp --sse AES256 "$PGP_TMP_FILE" "s3://$PGP_KEYS_BUCKET/$GIT_USER_NAME.asc" || true
 
 rm "$PGP_TMP_FILE"

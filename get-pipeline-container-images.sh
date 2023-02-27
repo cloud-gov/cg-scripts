209532389e2a11ac5fde2bc1fd2de40cc9c967a2
Mark Boyd <mark.boyd@gsa.gov>
2023-02-27 16:46:56 -0500

    add script to get all container pipeline images

---
+++ get-pipeline-container-images.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+function usage {
+  echo -e "
+  ./$( basename "$0" ) [pipeline-name] [--help, -h]
+
+  Get all container images used in Concourse pipelines
+
+  Optional environment variable \$CI_URL matching your Concourse URL.
+  example: CI_URL=https://ci.fr.cloud.gov ./$( basename "$0" )
+
+  Optional argument for specific pipeline to check
+  example: ./$( basename "$0" ) pipeline-name
+
+  \$CI_URL, Defaults to https://ci.fr.cloud.gov
+  "
+  exit
+}
+
+while getopts ":h" opt; do
+  case ${opt} in
+    h )
+        usage
+        exit 0
+        ;;
+    * )
+        usage
+        exit 0
+        ;;
+  esac
+done
+
+
+CI_URL="${CI_URL:-"https://ci.fr.cloud.gov"}"
+FLY_TARGET=$(fly targets | grep "${CI_URL}" | head -n 1 | awk '{print $1}')
+
+if ! fly --target "${FLY_TARGET}" workers > /dev/null; then
+  echo "Not logged in to concourse"
+  exit 1
+fi
+
+function find_resource_type_docker_images {
+  echo "$1" | jq -r 'select(has("resource_types")) | [.resource_types[] |
+        select(.type=="docker-image" or .type=="registry-image") |
+        .source |
+        .repository + (if .tag != null then ":" + (.tag|tostring) else "" end)] | unique'
+}
+
+function find_resource_docker_images {
+  echo "$1" | jq -r '[.resources[] |
+        select(.type=="docker-image" or .type=="registry-image") |
+        .source |
+        .repository + (if .tag != null then ":" + (.tag|tostring) else "" end)] | unique'
+}
+
+function find_task_docker_images {
+  echo "$1" \
+    | jq -r '[.jobs[].plan[] |
+        select(.config | has("image_resource")) |
+        select(.config.image_resource.type=="docker-image" or .config.image_resource.type=="registry-image") |
+        .config.image_resource.source |
+        .repository + (if .tag != null then ":" + (.tag|tostring) else "" end)] | unique'
+}
+
+function find_docker_images {
+  printf "\npipeline: %s\n\n" "$1" >&2
+  pipeline_json=$(fly -t ci get-pipeline --pipeline "$1" --json)
+  resource_type_images=$(find_resource_type_docker_images "$pipeline_json")
+  resource_images=$(find_resource_docker_images "$pipeline_json")
+  task_images=$(find_task_docker_images "$pipeline_json")
+  echo -e "$resource_type_images\n$resource_images\n$task_images" | jq -s 'add | unique | sort'
+}
+
+if [ -z "$1" ]; then
+  fly --target "${FLY_TARGET}" pipelines | tail -n +1 |  while read -r line; do
+      pipeline_name=$(echo "$line"  | awk '{print $2}')
+      
+      find_docker_images "$pipeline_name"
+  done
+else
+  find_docker_images "$1"
+fi
+

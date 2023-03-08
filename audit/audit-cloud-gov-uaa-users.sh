bed0390aa8912d7c1e1acae4eea756c244c58c1e
Mark Boyd <mark.boyd@gsa.gov>
2023-03-08 09:07:24 -0500

    update script for getting org admins

--- audit/audit-cloud-gov-uaa-users.sh
+++ audit/audit-cloud-gov-uaa-users.sh
@@ -9,14 +9,10 @@ for org in $orgs; do
   export all_org_users
 
   org_guid=$(cf org "$org" --guid)
-  org_users=$(cf curl "/v3/organizations/$org_guid/users" | jq -r '[.resources[] |
-    select(.username // empty | contains("gsa.gov")) |
-    .username]')
+  org_users=$(cf curl "/v3/organizations/$org_guid/users" | jq -r '[.resources[].username]')
   
   for space_guid in $(cf curl "/v3/spaces?organization_guids=$org_guid" | jq -r '.resources[].guid'); do
-    space_users=$(cf curl "/v3/spaces/$space_guid/users" | jq -r '[.resources[] |
-    select(.username // empty | contains("gsa.gov")) |
-    .username]')
+    space_users=$(cf curl "/v3/spaces/$space_guid/users" | jq -r '[.resources[].username]')
     echo "all cloud.gov admin users for org $org, space: $space_guid: $space_users" >&2
     all_org_users=$(echo -e "$org_users\n$space_users" | jq -s 'add | unique | sort')
   done
@@ -25,8 +21,4 @@ for org in $orgs; do
   all_admin_users=$(echo -e "$all_admin_users\n$all_org_users" | jq -s 'add | unique | sort')
 done
 
-echo "all cloud.gov admin users: $$all_admin_users" >&2
-
-for user in $(echo "$all_admin_users" | jq -r '.[]'); do
-  uaac user get "$user"
-done
+echo "$all_admin_users" | jq -r '.[]'

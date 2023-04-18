e51e9dd7810979f35d73956f977bab868512cc0f
Mark Boyd <mark.boyd@gsa.gov>
2023-04-18 11:11:33 -0400

    refactor script

--- get-pipeline-git-resource-branches.sh
+++ get-pipeline-git-resource-branches.sh
@@ -2,20 +2,35 @@
 
 function usage {
   echo -e "
-  ./$( basename "$0" ) [-p pipeline-name] [-r repo-name] [--help, -h]
+  ./$( basename "$0" ) [-p pipeline-name] [-r repo-names] [-b branch-name] [--help, -h]
 
   Get all git resources in Concourse pipelines that don\'t have commit signing
   configured
 
+  Options:
+    -p
+      Name of pipeline to search for git resources. No default value.
+
+    -r
+      Repo names to search for in git resources. Can provide single value (\"repo1\") or multiple values separated by pipe (e.g. \"repo1|repo2\").
+      Defaults to matching any repo name.
+
+    -b
+      Branch name to search for in git resources. Default value: \"master\"
+
   Examples:
-  # Find all uses of repo1 across all pipelines
-  ./$( basename "$0" ) repo1
 
-  # Find all uses of repo1 in a specific pipeline
-  ./$( basename "$0" ) repo1 pipeline1
+    # Find all uses of repo1 across all pipelines
+    ./$( basename "$0" ) -r repo1
 
-  # Find all uses of repo1 and repo2 across all pipelines
-  ./$( basename "$0" ) \"repo1|repo2\"
+    # Find all uses of repo1 across all pipelines
+    ./$( basename "$0" ) -r repo1
+
+    # Find all uses of repo1 in a specific pipeline
+    ./$( basename "$0" ) -p pipeline1 -r repo1
+
+    # Find all uses of repo1 and repo2 across all pipelines
+    ./$( basename "$0" ) -r \"repo1|repo2\"
 
   Optional environment variable \$CI_URL matching your Concourse URL.
   example: CI_URL=https://ci.fr.cloud.gov ./$( basename "$0" )
@@ -26,14 +41,18 @@ function usage {
 }
 
 REPO=".*"
+BRANCH="master"
 
-while getopts "r:p:h" opt; do
+while getopts "r:p:b:h" opt; do
   case ${opt} in
     r )
       REPO=${OPTARG}
       ;;
     p )
       PIPELINE=${OPTARG}
+      ;;
+    b )
+      BRANCH=${OPTARG}
       ;;
     h )
         usage
@@ -57,11 +76,11 @@ fi
 
 function find_git_resources_for_branch {
   fly -t ci get-pipeline --pipeline "$1" --json \
-    | jq --arg repo "$2" '.resources[] |
+    | jq --arg repo "$REPO" --arg branch "$BRANCH" '.resources[] |
         select(.type=="git") |
         select(.source.uri | test("github.com.*(cloud-gov|18[Ff])")) |
         select(.source.uri | test($repo)) |
-        select(.source.branch=="main")'
+        select(.source.branch==$branch)'
 }
 
 function find_git_resource_uris {
@@ -77,9 +96,9 @@ if [ -z "$PIPELINE" ]; then
   fly --target "${FLY_TARGET}" pipelines | tail -n +1 |  while read -r line; do
       pipeline_name=$(echo "$line"  | awk '{print $2}')
       
-      find_git_resource_uris "$pipeline_name" "$REPO"
+      find_git_resource_uris "$pipeline_name"
   done
 else
-  find_git_resource_uris "$PIPELINE" "$REPO"
+  find_git_resource_uris "$PIPELINE"
 fi
 

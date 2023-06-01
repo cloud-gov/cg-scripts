b4814e695521d3d069a60678fdfc740d70321e0e
Mark Boyd <mark.boyd@gsa.gov>
2023-06-01 15:39:16 -0400

    minor script updates

--- audit/audit-brokered-rds-storage.py
+++ audit/audit-brokered-rds-storage.py
@@ -20,25 +20,21 @@ def print_db_instances_csv_lines(instances):
         # Retrieve all of the tags associated with the instance.
         tags = { tag.get("Key"): tag.get("Value") for tag in db_instance["TagList"] }
 
-        org_guid = tags.get("Organization GUID", "")
-        space_guid = tags.get("Space GUID", "")
-        instance_guid = tags.get("Instance GUID", "")
-
         output = "{db_identifer},{instance_class},{storage_type},{storage_size},{instance_guid},{space_guid},{org_guid}".format(
             db_identifer=db_instance["DBInstanceIdentifier"],
             instance_class=db_instance["DBInstanceClass"],
             storage_type=db_instance["StorageType"],
             storage_size=db_instance["AllocatedStorage"],
-            instance_guid=instance_guid,
-            space_guid=space_guid,
-            org_guid=org_guid,
+            instance_guid=tags.get("Instance GUID", ""),
+            space_guid=tags.get("Space GUID", ""),
+            org_guid=tags.get("Organization GUID", ""),
         )
 
         print(output)
 
 def print_rds_database_csv_header():
   print("Database ID,Instance Class,Storage Type,Storage Size,Instance GUID,Space GUID,Organization GUID")
-  
+
 def print_rds_database_audit_csv():
   print_rds_database_csv_header()
   print_all_db_instances_csv_lines()

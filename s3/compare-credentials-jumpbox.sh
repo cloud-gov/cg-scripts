34a1a47f36d67817cb8cb51bd2996f992755f957
Jason Gambino <jason.gambino3@gmail.com>
2023-07-05 08:35:25 -0400

    Update s3/compare-credentials-jumpbox.sh
    Co-authored-by: Mark Boyd <mark.boyd@gsa.gov>

--- s3/compare-credentials-jumpbox.sh
+++ s3/compare-credentials-jumpbox.sh
@@ -1,9 +1,10 @@
 # Pass the credential file as an argument
 main() {
     CREDENTIAL_FILE=$(basename $1)
-    aws s3 cp --sse AES256 s3://concourse-credentials/$CREDENTIAL_FILE test-file-s3.yml
-    diff $1 test-file-s3.yml
-    rm test-file-s3.yml
+    tmp_file=$(mktemp)
+    aws s3 cp --sse AES256 s3://concourse-credentials/$CREDENTIAL_FILE $tmp_file
+    diff $1 $tmp_file
+    rm $tmp_file
     echo "do you want to upload? y/n"
     read response
     if [[ $response = "y" ]]

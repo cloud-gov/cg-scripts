fe34e19f81bd87b2d1e49bc8b6a5e0d31628c96c
Jason A. Gambino <jason.gambino@gsa.gov>
2023-07-05 10:01:56 -0400

    updating with recommendations for better scripts

--- s3/compare-credentials.sh
+++ s3/compare-credentials.sh
@@ -1,14 +1,14 @@
 # Pass the credential file as an arguments
 main() {
     CREDENTIAL_FILE=$(basename $1)
-    aws-vault exec gov-prd-plat-admin -- aws s3 cp --sse AES256 s3://concourse-credentials/$CREDENTIAL_FILE test-file-s3.yml
+    aws s3 cp --sse AES256 s3://concourse-credentials/$CREDENTIAL_FILE test-file-s3.yml
     diff $1 test-file-s3.yml
     rm test-file-s3.yml
     echo "do you want to upload? y/n"
     read response
     if [[ $response =  "y" ]]
     then
-      aws-vault exec gov-prd-plat-admin -- aws s3 cp --sse AES256 $1 s3://concourse-credentials/$CREDENTIAL_FILE
+      aws s3 cp --sse AES256 $1 s3://concourse-credentials/$CREDENTIAL_FILE
     fi
 }
 
--- s3/update-credentials.sh
+++ s3/update-credentials.sh
@@ -1,9 +1,9 @@
 # Pass the credential file as an argument
 main() {
     CREDENTIAL_FILE=$(basename $1)
-    aws-vault exec gov-prd-plat-admin -- aws s3 cp --sse AES256 s3://concourse-credentials/$CREDENTIAL_FILE "2${CREDENTIAL_FILE}"
-    rm $1
-    mv "2${CREDENTIAL_FILE}" $CREDENTIAL_FILE
+    tmpfile=$(mktemp)
+    aws s3 cp --sse AES256 s3://concourse-credentials/$CREDENTIAL_FILE "$tmpfile"
+    mv "$tmpfile" $CREDENTIAL_FILE
 }
 
 main "$@"

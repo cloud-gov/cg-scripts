328458868697446d9513bea38d44afe652bcaa94
Mark Boyd <mark.boyd@gsa.gov>
2023-10-03 10:52:10 -0400

    revert changes

--- aws/ec2/get-ec2-volume-instance-tags.sh
+++ aws/ec2/get-ec2-volume-instance-tags.sh
@@ -13,13 +13,8 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-TAGS="Name,deployment"
-TAGS_FILTER=$(IFS=','; for tag in $TAGS; do echo \"$tag\"; done)
-
-echo $TAGS_FILTER
-
-# for instance_id in $(aws ec2 describe-volumes --filters="Name=$1,Values=$2" --output json | jq -r '.Volumes[].Attachments[].InstanceId'); do
-#   aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" | jq '.Tags[] | select(.Key==("Name", "deployment")) | .Key + ":" + .Value'
-#   printf "\n"
-# done
+for instance_id in $(aws ec2 describe-volumes --filters="Name=$1,Values=$2" --output json | jq -r '.Volumes[].Attachments[].InstanceId'); do
+  aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" | jq '.Tags[] | select(.Key==("Name", "deployment")) | .Key + ":" + .Value'
+  printf "\n"
+done
 

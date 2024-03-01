0cdaa924e123a517a4105ca61a94331f468753bf
Mark Boyd <mark.boyd@gsa.gov>
2024-03-01 17:41:47 -0500

    Update concourse scripts (#300)
    * create concourse script to get configuration for pipelines
    
    * move scripts into concourse folder
    
    * update gitignore to exclude download pipeline configuration
    
    * update output path for downloaded pipelines to be in concourse folder

---
+++ concourse/get-pipelines.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+function usage {
+  echo -e "
+  ./$( basename "$0" ) [pipeline-name] [--help, -h]
+
+  Get definitions for Concourse pipeline(s)
+
+  Downloads configuration YAML files for Concourse pipeline(s)
+
+  Optional environment variable \$CI_URL matching your Concourse URL.
+  example: CI_URL=https://ci.fr.cloud.gov ./$( basename "$0" )
+
+  Optional argument for specific pipeline to check
+  example: ./$( basename "$0" ) pipeline-name
+
+  \$CI_URL, Defaults to https://ci.fr.cloud.gov
+  "
+  exit
+}
+
+while getopts ":h" opt; do
+  case ${opt} in
+    h )
+        usage
+        exit 0
+        ;;
+    * )
+        usage
+        exit 0
+        ;;
+  esac
+done
+
+CI_URL="${CI_URL:-"https://ci.fr.cloud.gov"}"
+FLY_TARGET=$(fly targets | grep "${CI_URL}" | head -n 1 | awk '{print $1}')
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if ! fly --target "${FLY_TARGET}" workers > /dev/null; then
+  echo "Not logged in to concourse"
+  exit 1
+fi
+
+function get_pipeline {
+  printf "getting pipeline: %s\n" "$1" >&2
+  fly --target "${FLY_TARGET}" get-pipeline -p "$1" > "$SCRIPT_DIR/$1.yml"
+}
+
+if [ -z "$1" ]; then
+  for pipeline_name in $(fly --target "${FLY_TARGET}" pipelines | tail -n +1 | awk '{print $2}');
+  do
+    get_pipeline "$pipeline_name"
+  done
+else
+  get_pipeline "$1"
+fi
+

5a5082103dc068faa85b9f3524e0ddb57bdb13a8
Mark Boyd <mark.boyd@gsa.gov>
2024-03-19 12:41:47 -0400

    Update send email script (#305)
    * update gitignore to ignore all message files
    
    * update example template
    
    * update example addresses
    
    * modify send.sh to allow parsing and replacing more custom data in templates
    
    * update script instructions
    
    * update template with example org and space values
    
    * update template

--- email/send.sh
+++ email/send.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 # https://github.com/cloud-gov/internal-docs/tree/main/docs/runbooks/Customer-Communication/email-customers.md
 
 if [ ! -f addresses.txt ]; then
-    echo "To run, create addresses.txt with one email address per line. See example-addresses.txt."
+    echo "To run, create addresses.txt with one email address per line. Other data can follow the email address separated by \";\". See example-addresses.txt."
     exit 1
 fi
 
@@ -19,21 +19,36 @@ if [ ! -f template.html ]; then
 fi
 
 if ! msmtp --version &> /dev/null; then
-	echo "send.sh requires msmtp to be installed. See https://github.com/internal-docs/tree/main/docs/runbooks/Customer-Communication/bulk-email.md for full setup instructions."
+	echo "send.sh requires msmtp to be installed. See https://github.com/cloud-gov/internal-docs/blob/main/docs/runbooks/Customer-Communication/email-customers.md for full setup instructions."
 	exit 1
 fi
 
-if [ ! -f $HOME/.msmtprc ]; then
-	echo "To run, create ~/.msmtprc. See https://github.com/internal-docs/tree/main/docs/runbooks/Customer-Communication/bulk-email.md for full setup instructions."
+if [ ! -f "$HOME/.msmtprc" ]; then
+	echo "To run, create ~/.msmtprc. See https://github.com/cloud-gov/internal-docs/blob/main/docs/runbooks/Customer-Communication/email-customers.md for full setup instructions."
 	exit 1
 fi
 
-while read line; do
-	cp template.html message.html
-	# edit message.html in-place to replace the "%" placeholder with the recipient address.
-	sed -e "s/%/${line}/g" -i "" message.html
-	msmtp -t < ./message.html
+while read -r line; do
+  IFS=';' read -r -a array <<< "$line"
+
+	MESSAGE="message.html"
+
+	cp template.html "$MESSAGE"
+
+	# edit message.html in-place to replace the "%email" placeholder with the recipient address.
+	EMAIL="${array[0]}"
+	sed -e "s/%email/${EMAIL}/g" -i "" "$MESSAGE"
+
+	# this is an example of how you can replace "%space" in the template.html file with more custom data
+	SPACE_NAME="${array[1]}"
+	sed -e "s/%space/${SPACE_NAME}/g" -i "" "$MESSAGE"
+
+	# this is an example of how you can replace "%org" in the template.html file with more custom data
+	ORG_NAME="${array[2]}"
+	sed -e "s/%org/${ORG_NAME}/g" -i "" "$MESSAGE"
+
+	msmtp -t < "$MESSAGE"
 done <addresses.txt
 
 # clean up
-rm message.html
+rm message*.html

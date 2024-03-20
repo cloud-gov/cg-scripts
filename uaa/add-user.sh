8fc09f4cd8eb598c30adee4468650df2755449e0
Mark Boyd <mark.boyd@gsa.gov>
2024-03-20 14:10:22 -0400

    add script for creating users in UAA (#306)
    * add script for creating users
    
    * document how origin is determined by script

---
+++ uaa/add-user.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -lt 1 ]; then
+  echo
+  echo "Usage:"
+  echo "  $ ./add-user.sh <EMAIL_ADDRESS>"
+  echo
+  echo "Note: the user is added to UAA with an origin of their email domain"
+  echo
+  exit 1;
+fi
+
+if ! hash uaac 2>/dev/null; then
+  echo -n "uaac must be installed"
+  exit 1
+fi
+
+EMAIL=$1
+ORIGIN=$(awk -F '@' '{print $2}' <<< "$EMAIL")
+
+echo -n "Adding user ${EMAIL}... "
+uaac curl \
+  -X POST /Users \
+  -H "If-Match:*" \
+  -H "Accept:application/json" \
+  -H "Content-Type:application/json" \
+  -d \{\"userName\":\""${EMAIL}"\",\"emails\":[\{\"value\":\""${EMAIL}"\"\}],\"active\":true,\"verified\":true,\"origin\":\""${ORIGIN}"\"\}
+

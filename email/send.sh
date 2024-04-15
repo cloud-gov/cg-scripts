bf8c064d9dad847193aa7bf7184eec36ba9ca168
Mark Boyd <mark.boyd@gsa.gov>
2024-04-15 14:41:43 -0400

    Add sandbox restore script (#318)
    * add script for restoring purged sandboxes that do not get recreated correctly
    
    * adjust expected data format for customer email script

--- email/send.sh
+++ email/send.sh
@@ -35,17 +35,17 @@ while read -r line; do
 
 	cp template.html "$MESSAGE"
 
-	# edit message.html in-place to replace the "%email" placeholder with the recipient address.
-	EMAIL="${array[0]}"
-	sed -e "s/%email/${EMAIL}/g" -i "" "$MESSAGE"
-
 	# this is an example of how you can replace "%space" in the template.html file with more custom data
-	SPACE_NAME="${array[1]}"
+	SPACE_NAME="${array[0]}"
 	sed -e "s/%space/${SPACE_NAME}/g" -i "" "$MESSAGE"
 
 	# this is an example of how you can replace "%org" in the template.html file with more custom data
-	ORG_NAME="${array[2]}"
+	ORG_NAME="${array[1]}"
 	sed -e "s/%org/${ORG_NAME}/g" -i "" "$MESSAGE"
+
+	# edit message.html in-place to replace the "%email" placeholder with the recipient address.
+	EMAIL="${array[2]}"
+	sed -e "s/%email/${EMAIL}/g" -i "" "$MESSAGE"
 
 	msmtp -t < "$MESSAGE"
 done <addresses.txt

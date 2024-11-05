e88c7c23f1009c20e800a1500e7fef473ea8591f
Christopher Weibel <christopher.weibel@gsa.gov>
2024-11-05 16:23:10 -0500

    Testing

--- credhub/scriptme.sh
+++ credhub/scriptme.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e -u
 export CREDHUB_CA_CERT="$(pwd)/${CREDHUB_CA_CERT}"
+
+cat << EOF > slots.value
+SLOT1=$(credhub get -n /concourse/pages/com-waf-search-string-pages-slot-1 -q)
+SLOT2=$(credhub get -n /concourse/pages/com-waf-search-string-pages-slot-2 -q)
+EOF
+
 exit 1

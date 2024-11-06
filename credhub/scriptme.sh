3c9424a20a7d6595549c75c9722357ab388b4c60
Christopher Weibel <christopher.weibel@gsa.gov>
2024-11-06 11:36:16 -0500

    Testing

--- credhub/scriptme.sh
+++ credhub/scriptme.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e -u
-export CREDHUB_CA_CERT="$(pwd)/${CREDHUB_CA_CERT}"
+#export CREDHUB_CA_CERT="$(pwd)/${CREDHUB_CA_CERT}"
 
+# Load up current values
+echo "Looking up Pages values..."
 PAGES_SLOT_1="$(credhub get -n /concourse/pages/com-waf-search-string-pages-slot-1 -q)"
 PAGES_SLOT_2="$(credhub get -n /concourse/pages/com-waf-search-string-pages-slot-2 -q)"
 
-MAIN_SLOT_1="$(credhub get -n /concourse/main/com-waf-search-string-pages-slot-1 -q)"
-MAIN_SLOT_2="$(credhub get -n /concourse/main/com-waf-search-string-pages-slot-2 -q)"
-
-
+echo "Looking up Main values..."
+MAIN_SLOT_1="$(credhub get -n /concourse/main/aws-admin/com-waf-search-string-pages-slot-1 -q)"
+MAIN_SLOT_2="$(credhub get -n /concourse/main/aws-admin/com-waf-search-string-pages-slot-2 -q)"
+               
 # Compare variables
+echo  "Performing compares..."
 if [[ "$PAGES_SLOT_1" != "$MAIN_SLOT_1" ]]; then
     echo "SLOT 1 is different. Updating main with the value of pages."
     credhub set -n /concourse/main/com-waf-search-string-pages-slot-1 -t value -v "${PAGES_SLOT_1}"
@@ -20,7 +23,7 @@ fi
 # Compare variables
 if [[ "$PAGES_SLOT_2" != "$MAIN_SLOT_2" ]]; then
     echo "SLOT 2 is different. Updating main with the value of pages."
-    credhub set -n /concourse/main/com-waf-search-string-pages-slot-2 -t value -v "${PAGES_SLOT_2}"
+    credhub set -n /concourse/main/aws-admin/com-waf-search-string-pages-slot-2 -t value -v "${PAGES_SLOT_2}"
 else
     echo "SLOT 2 matches. No changes needed."
 fi

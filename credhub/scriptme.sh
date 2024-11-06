a9368d67c5dbf7886f321eb322b3ecbb576534bf
Christopher Weibel <christopher.weibel@gsa.gov>
2024-11-06 12:04:28 -0500

    Testing

--- credhub/scriptme.sh
+++ credhub/scriptme.sh
@@ -15,7 +15,7 @@ MAIN_SLOT_2="$(credhub get -n /concourse/main/aws-admin/com-waf-search-string-pa
 echo  "Performing compares..."
 if [[ "$PAGES_SLOT_1" != "$MAIN_SLOT_1" ]]; then
     echo "SLOT 1 is different. Updating main with the value of pages."
-    credhub set -n /concourse/main/com-waf-search-string-pages-slot-1 -t value -v "${PAGES_SLOT_1}"
+    credhub set -n /concourse/main/aws-admin/com-waf-search-string-pages-slot-1 -t value -v "${PAGES_SLOT_1}"
 else
     echo "SLOT 1 matches. No changes needed."
 fi
@@ -23,7 +23,7 @@ fi
 # Compare variables
 if [[ "$PAGES_SLOT_2" != "$MAIN_SLOT_2" ]]; then
     echo "SLOT 2 is different. Updating main with the value of pages."
-    credhub set -n /concourse/main/aws-admin/com-waf-search-string-pages-slot-2 -t value -v "${PAGES_SLOT_2}"
+    credhub set -n /concourse/main/aws-admin/aws-admin/com-waf-search-string-pages-slot-2 -t value -v "${PAGES_SLOT_2}"
 else
     echo "SLOT 2 matches. No changes needed."
 fi

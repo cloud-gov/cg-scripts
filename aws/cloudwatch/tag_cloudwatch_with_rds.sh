1031c1295756914bcbba8dc78ea6c899268ba9c9
Mark Boyd <mark.boyd@gsa.gov>
2024-12-05 14:01:07 -0500

    add check for whether database exists before attempting to get tags

--- aws/cloudwatch/tag_cloudwatch_with_rds.sh
+++ aws/cloudwatch/tag_cloudwatch_with_rds.sh
@@ -7,14 +7,21 @@ for instance in $log_groups; do
     TAG=${instance#/aws/rds/instance/}
     TAG=${TAG%/*}
     ARN="arn:aws-us-gov:rds:${AWS_REGION}:${ACCOUNT_ID}:db:${TAG}"
-    RDS_TAGS=$(aws rds list-tags-for-resource --resource-name ${ARN} --query "TagList" --output json)
+
+    if ! aws rds describe-db-instances --db-instance-identifier "$ARN" > /dev/null; then
+        echo "no database instance for $ARN" >&2
+        continue
+    fi
+
+    RDS_TAGS=$(aws rds list-tags-for-resource --resource-name "$ARN" --query "TagList" --output json)
     TAGS_STRING=$(echo "$RDS_TAGS" | jq -r '.[] | "\(.Key)=\(.Value)"' | paste -sd ',' -)
     TAGS_STRING=${TAGS_STRING// /}
+
     if [ -z "$RDS_TAGS" ]; then
         echo "invalid tags for ${instance}"
     else
-        echo "Success on ${instance}"
         aws logs tag-log-group --log-group-name "${instance}" --tags "$TAGS_STRING"
+        echo "Success on ${instance}"
     fi
 done
 

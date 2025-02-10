d2a65800581f234d80ae742a64586f33e2a874db
Peter Burkholder <peter.burkholder@gsa.gov>
2025-02-10 14:07:48 -0500

    De-minimis archiving scripts

---
+++ pages/archive-sites.sh
@@ -0,0 +1,39 @@
+#! /bin/bash 
+
+set -e
+
+function fail() {
+	echo $@
+	exit 1
+}
+
+function mirror() {
+	set -x
+	local site=$1
+	 wget --recursive \
+	       --directory-prefix="$PREFIX" \
+           --page-requisites \
+           --adjust-extension \
+           --span-hosts \
+           --convert-links \
+           --domains $site \
+           --no-parent https://www.$site
+	set +x
+}
+
+TARGET="$HOME/Google Drive/Shared drives/Cloud.gov Public/IDCDB-archive"
+
+[ -d "$TARGET" ] || fail "Cannot access Google Drive target $TARGET"
+
+TIMESTAMP=$(date "+%Y%m%d-%H%M")
+
+PREFIX="$TARGET/$TIMESTAMP" 
+mkdir -p "$PREFIX" || fail "Unable to create the target directory $PREFIX"
+
+for S in cdo.gov cfo.gov paymentaccuracy.gov cio.gov coffa.gov evaluation.gov fpc.gov statspolicy.gov fcsm.gov; do 
+  echo "=============="
+  echo "STARTING SITE: $S"
+  echo "=============="
+  sleep 1
+  mirror $S 
+done

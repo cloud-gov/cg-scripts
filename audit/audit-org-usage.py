243b0d2534c6e5e6a9f4d449af027617f472f9eb
Peter Burkholder <peter.burkholder@gsa.gov>
2025-04-09 16:12:34 -0400

    Emits ARNs

--- audit/audit-org-usage.py
+++ audit/audit-org-usage.py
@@ -3,14 +3,21 @@
 import subprocess
 import sys
 import json
+import boto3
 
-# Option 1: Using a class
+tags_client = boto3.client('resourcegroupstaggingapi')
+
+class Rds:
+    def __init__(self, arn):
+        self.arn = arn
+
 class Organization:
     def __init__(self, name):
         self.name = name
         self.data = self.get_data()
         self.guid = self.data['guid']
         self.quota_guid = self.data['relationships']['quota']['data']['guid']
+        self.rds_instances = []
 
     def get_data(self):
         cf_json = subprocess.check_output(
@@ -35,6 +42,35 @@ class Organization:
             shell=True,
         )
         return json.loads(cf_json)['usage_summary']['memory_in_mb']
+    
+    def get_rds_instances(self, client):
+        tag_key = "Organization GUID"
+        tag_value = self.guid
+        response = client.get_resources(
+            TagFilters = [
+                {
+                    'Key': tag_key,
+                    'Values': [tag_value]
+        
+                }
+            ]
+        )
+        for resource in response['ResourceTagMappingList']:
+            r = resource['ResourceARN']
+            # rds = Rds(response['ResourceARN'])
+            self.rds_instances.append(r)
+
+#{ "ResourceTagMappingList": [ {
+#            "ResourceARN": "arn:aws-us-gov:rds:us-gov-west-1:135676904304:db:cg-aws-broker-prodme3mx7or6nhflbj",
+#            "Tags": [
+#                {
+#                    "Key": "Service offering name",
+#                    "Value": "aws-rds"
+#                },
+#                {
+#                    "Key": "Organization GUID",
+#                    "Value": "77fa4cb4-963d-491d-ac73-23b6de945edd"
+#                },
 
 
 def test_authenticated():
@@ -56,11 +92,14 @@ def test_authenticated():
 def main():
     # test_authenticated()
     org = Organization(name="cloud-gov-operators")
+    org.get_rds_instances(tags_client)
+
     print(f"Organization name: {org.name}")
     print(f"Organization GUID: {org.guid}")
     print(f"Organization memory quota: {org.get_quota_memory()}")
     print(f"Organization memory usage: {org.get_memory_usage()}")
-
+    for r in org.rds_instances:
+        print(f"RDS ARN: {r}")
 
 if __name__ == "__main__":
     main()

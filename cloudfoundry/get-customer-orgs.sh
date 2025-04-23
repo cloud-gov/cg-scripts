bb9850381a5ff57daeec21e827e972c0e660ba06
Mark Boyd <mark.boyd@gsa.gov>
2025-04-23 17:48:16 -0400

    add script to get all platform customer orgs (#352)
    * add script to get all platform customer orgs
    
    * fix script
    
    * update get customer orgs script to fetch by label
    
    * update script to return org names and sort results

---
+++ cloudfoundry/get-customer-orgs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CUSTOMER_ORG_FILTER=$(echo "org-type=customer" | jq -Rr @uri)
+
+# NOTE: does not handle paging
+cf curl "/v3/organizations?per_page=5000&label_selector=$CUSTOMER_ORG_FILTER" | jq -r '.resources[].name' | sort

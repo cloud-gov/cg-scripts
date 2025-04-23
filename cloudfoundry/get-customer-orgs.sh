a292b4a5b3d4b4d8189f19783d7e4339d38efabc
Mark Boyd <mark.boyd@gsa.gov>
2025-04-23 15:52:12 -0400

    update script to return org names and sort results

--- cloudfoundry/get-customer-orgs.sh
+++ cloudfoundry/get-customer-orgs.sh
@@ -3,4 +3,4 @@
 CUSTOMER_ORG_FILTER=$(echo "org-type=customer" | jq -Rr @uri)
 
 # NOTE: does not handle paging
-cf curl "/v3/organizations?per_page=5000&label_selector=$CUSTOMER_ORG_FILTER"
+cf curl "/v3/organizations?per_page=5000&label_selector=$CUSTOMER_ORG_FILTER" | jq -r '.resources[].name' | sort

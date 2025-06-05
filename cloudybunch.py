0ec5a2ecf828be4ffdef4c11a9dd5af12e3ad082
Peter Burkholder <peter.burkholder@gsa.gov>
2025-06-05 12:13:11 -0400

    Fixed spacing, fix yuda, full 8 offset x 2

--- cloudybunch.py
+++ cloudybunch.py
@@ -43,16 +43,16 @@ team = [
     ":steve-greenberg:",
     ":sven:",
     ":van-nguyen:",
-    ":yuda"
+    ":yuda:"
 ]
 # randomize the input list, t,
 # then print samples with spacing n
 def bunch(t,n):
     bunch = random.sample(t,len(t))
     for n in range(0,len(t)+n,n):
-        print(f':musical_note: {bunch[(n+0)%len(bunch)]} {bunch[(n+1)%len(bunch)]} {bunch[(n+2)%len(bunch)]}\\n',
-            f' :blank: {bunch[(n+3)%len(bunch)]} :cloud-gov: {bunch[(n+4)%len(bunch)]} \\n',
-            f' :blank: {bunch[(n+5)%len(bunch)]} {bunch[(n+6)%len(bunch)]} {bunch[(n+7)%len(bunch)]} :musical_note:')
+        print(f':musical_note:{bunch[(n+0)%len(bunch)]}{bunch[(n+1)%len(bunch)]}{bunch[(n+2)%len(bunch)]}\\n'
+            f':blank:{bunch[(n+3)%len(bunch)]}:cloud-gov:{bunch[(n+4)%len(bunch)]}\\n'
+            f':blank:{bunch[(n+5)%len(bunch)]}{bunch[(n+6)%len(bunch)]}{bunch[(n+7)%len(bunch)]}:musical_note:')
 
-bunch(team,2)
-bunch(team,2)
+bunch(team,8)
+bunch(team,8)

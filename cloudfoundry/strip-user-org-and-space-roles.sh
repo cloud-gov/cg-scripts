bbd2cdadf5a81b5ff9f483ec1e8f8cf2f967ef8b
Mark Boyd <mark.boyd@gsa.gov>
2025-12-02 17:33:20 -0500

    Improve script removing cf access (#375)
    * improve script for removing user access from a CF org
    
    * remove org name from script

--- cloudfoundry/strip-user-org-and-space-roles.sh
+++ cloudfoundry/strip-user-org-and-space-roles.sh
@@ -4,22 +4,24 @@ set -euo pipefail
 shopt -s inherit_errexit || true
 
 main() {
-  [[ $# -eq 3 ]] || usage "Expected three arguments, got $#"
+  [[ $# -eq 2 ]] || usage "Expected two arguments, got $#"
 
   local user="$1"
   local org="$2"
-  local space="$3"
 
   cf unset-org-role "$user" "$org" OrgManager
-  for space_role in SpaceManager SpaceDeveloper SpaceAuditor; do
-    cf unset-space-role "$user" "$org" "$space" "$space_role"
-  done
 
   echo "Org users:"
   cf org-users "$org"
 
-  echo "Space users:"
-  cf space-users "$org" "$space"
+  for space in $(cf curl "/v3/spaces?organization_guids=$(cf org $org --guid)" | jq -r '.resources[].name'); do
+    for space_role in SpaceManager SpaceDeveloper SpaceAuditor; do
+      cf unset-space-role "$user" "$org" "$space" "$space_role"
+    done
+
+    echo "Space users:"
+    cf space-users "$org" "$space"
+  done
 }
 
 usage() {

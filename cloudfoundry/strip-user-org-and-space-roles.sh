9345d5c3ce29c4f6aadbdd36100b74c9cb0e4f3c
Peter Burkholder <peter.burkholder@gsa.gov>
2026-06-29 17:47:13 -0400

    Remove space roles before org role

--- cloudfoundry/strip-user-org-and-space-roles.sh
+++ cloudfoundry/strip-user-org-and-space-roles.sh
@@ -18,8 +18,24 @@ main() {
 
   ORGANIZATION_GUID=$(cf org "$org" --guid)
 
-  # get all user roles for the org, including organization_user
-  for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&organization_guids=$ORGANIZATION_GUID&per_page=5000" | jq -r '.resources[].guid'); do
+  # get all space guids belonging to the org
+  SPACE_GUIDS=$(cf curl "/v3/spaces?organization_guids=$ORGANIZATION_GUID" | jq -r '.resources[].guid')
+
+  if [[ -n "$SPACE_GUIDS" ]]; then
+    local space_guids_csv
+    space_guids_csv=$(echo "$SPACE_GUIDS" | paste -sd, -)
+    # limit to 20 role lest we have a bug later
+    for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&space_guids=$space_guids_csv&per_page=20" | jq -r '.resources[].guid'); do
+      echo cf curl -X DELETE "/v3/roles/$role_guid"
+      sleep 1
+      cf curl -X DELETE "/v3/roles/$role_guid"
+    done
+  fi
+
+  # now remove all org-level roles for the user, including organization_user
+  for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&organization_guids=$ORGANIZATION_GUID&per_page=1" | jq -r '.resources[].guid'); do
+    # Let the above role removals propagate (sleep 1 was insufficent)
+    sleep 5
     cf curl -X DELETE "/v3/roles/$role_guid"
   done
 }

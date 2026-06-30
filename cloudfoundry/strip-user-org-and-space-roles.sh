04dcf73fc232d3847f52616399c012c0828d566a
Peter Burkholder <peter.burkholder@gsa.gov>
2026-06-30 10:35:55 -0400

    More user feedback, catch no org

--- cloudfoundry/strip-user-org-and-space-roles.sh
+++ cloudfoundry/strip-user-org-and-space-roles.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 shopt -s inherit_errexit || true
 
-MAX_SPACE_ROLES=40
-MAX_ORG_ROLES=8
+MAX_SPACE_ROLES=100
+MAX_ORG_ROLES=20
 
 main() {
   [[ $# -eq 2 ]] || usage "Expected two arguments, got $#"
@@ -19,7 +19,10 @@ main() {
     exit 1
   fi
 
-  ORGANIZATION_GUID=$(cf org "$org" --guid)
+  if ! ORGANIZATION_GUID=$(cf org "$org" --guid); then
+    echo "no org found for $org. Either it doesn't exist or something else went wrong"
+    exit 1
+  fi
 
   # get all space guids belonging to the org
   SPACE_GUIDS=$(cf curl "/v3/spaces?organization_guids=$ORGANIZATION_GUID" | jq -r '.resources[].guid')
@@ -27,16 +30,16 @@ main() {
   if [[ -n "$SPACE_GUIDS" ]]; then
     local space_guids_csv
     space_guids_csv=$(echo "$SPACE_GUIDS" | paste -sd, -)
-    # limit to 20 role lest we have a bug later
-    for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&space_guids=$space_guids_csv&per_page=${MAX_SPACE_ROLES}" | jq -r '.resources[].guid'); do
-      echo cf curl -X DELETE "/v3/roles/$role_guid"
-      sleep 1
+
+    for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&space_guids=$space_guids_csv&per_page=$MAX_SPACE_ROLES" | jq -r '.resources[].guid'); do
+      echo removing space role with: cf curl -X DELETE "/v3/roles/$role_guid"
       cf curl -X DELETE "/v3/roles/$role_guid"
     done
   fi
 
   # now remove all org-level roles for the user, including organization_user
-  for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&organization_guids=$ORGANIZATION_GUID&per_page=${MAX_ORG_ROLES}" | jq -r '.resources[].guid'); do
+  for role_guid in $(cf curl "/v3/roles?user_guids=$USER_GUID&organization_guids=$ORGANIZATION_GUID&per_page=$MAX_ORG_ROLES" | jq -r '.resources[].guid'); do
+    echo removing org role with: cf curl -X DELETE "/v3/roles/$role_guid"
     # Let the above role removals propagate (sleep 1 was insufficent)
     sleep 5
     cf curl -X DELETE "/v3/roles/$role_guid"
